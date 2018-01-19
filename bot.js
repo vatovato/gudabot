43641f7b1383e7b1d61bc5a904dfaecfb12705c3
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-18 22:13:50 -0300

    express and ejs
    Adds express and ejs to prevent Heroku dynos from falling asleep every 30 minutes

--- bot.js
+++ bot.js
@@ -1,6 +1,36 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+// Adds express to bypass Heroku's 30 minutes sleep
+const express = require('express.js');
+const app = express();
+
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// Make express look in the 'public' directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+    // ejs render automatically looks in the views folder
+    response.render('index');
+});
+
+app.listen(port, () => {
+    // will echo 'Our app is running on http://localhost:5000 when run locally'
+    console.log('Our app is running on http://localhost:' + port);
+});
+
+// pings server every 15 minutes to prevent dynos from sleeping
+setInterval(() => {
+ http.get('https://gudabot.herokuapp.com');
+}, 900000);
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
@@ -29,7 +59,7 @@ client.on("message", message => {
         console.error(err);
     }
 
-    
+
 });
 
 client.login(process.env.BOT_TOKEN);
