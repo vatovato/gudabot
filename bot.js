6ef63033edbcb1f0c52ab22b59f904b699796f02
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-18 22:54:05 -0300

    Express error
    Corrected require('express.js') to require ('express')

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
 // Adds express to bypass Heroku's 30 minutes sleep
-const express = require('express.js');
+const express = require('express');
 const app = express();
 
 
