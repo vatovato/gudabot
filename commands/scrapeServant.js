3a5a21686292532bc43ee37dd2579f403a1fe002
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-19 12:36:20 -0300

    more corrections to scrapeServant

--- commands/scrapeServant.js
+++ commands/scrapeServant.js
@@ -20,8 +20,8 @@ request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, res
   if(!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     var table = $('table.sortable');
-    var tableIconBody = $(table).find('a').attr(`href="#${urlRef}"`).toArray();
-    console.log(tableIconBody);
+    var tableIconBody = $(table).find('a');
+    console.log(tableIconBody.html());
   }
 });
 
