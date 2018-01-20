ddfee3ab10ba2c9b36f0605c90169c5a49a9ad6f
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-20 20:24:03 -0300

    asd

--- commands/image.js
+++ commands/image.js
@@ -10,14 +10,13 @@ exports.run = (client, message, args) => {
     var callSwitch  = require(`./switch.js`);
     urlRef = callSwitch.parseName(servantName);
     console.log(`urlRef after switch: ${urlRef}`);
-/*
-    if (urlRef === '') {
+
+    if (urlRef.length == 0) {
       message.channel.send("You haven't written a valid Servant name");
     } else {
-    //message.channel.send('Link to Cirno: http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef);
     servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef
     console.log(`URL for Servant: ${servantUrl}`);
     let commandFile2 = require(`./scrapeImage.js`);
     commandFile2.run(servantUrl, client, message);
-  }*/
+  }
   }
