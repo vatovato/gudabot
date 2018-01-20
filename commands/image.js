e3a4985e9a2d1c2523836b7b2c646546fa046d06
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-20 20:12:50 -0300

    asd
    asd

--- commands/image.js
+++ commands/image.js
@@ -1,4 +1,3 @@
-
 exports.run = (client, message, args) => {
     var servantName = args.join(" ").toLowerCase();
     var urlRef = '';
@@ -7,7 +6,7 @@ exports.run = (client, message, args) => {
     var message = message;
     console.log(`Required image for ${servantName}`);
 
-    console.log(`urlRef before switch: ${urlRef}`);
+    /* console.log(`urlRef before switch: ${urlRef}`);
     var callSwitch  = require(`./switch.js`);
     urlRef = callSwitch.parseName(servantName);
     console.log(`urlRef after switch: ${urlRef}`);
@@ -20,10 +19,5 @@ exports.run = (client, message, args) => {
     console.log(`URL for Servant: ${servantUrl}`);
     let commandFile2 = require(`./scrapeImage.js`);
     commandFile2.run(servantUrl, client, message);
-    }
+  }*/
   }
-    /* let age = args[0];
-    let sex = args[1];
-    let location = args[2];
-    message.channel.send(`Hello, ${message.author.username}, I see you're a ${age} old ${sex} from ${location}. Wanna date?`).catch(console.error);
-*/
