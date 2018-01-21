ad99078c33a389d48ed8094d9f92143b4f3352f8
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-21 20:41:53 -0300

    asd

--- commands/master.js
+++ commands/master.js
@@ -1,19 +1,13 @@
+exports.run = (client, message, args) => {
 var request = require('request');
 var cheerio = require('cheerio');
 const Discord = require('discord.js');
 
 
-//Servant details. Name is name, ascensions is an array with the ascension tier,
-//QP cost and materials needed for every tier
-var servant = {
-  name: '',
-  ascensions: []
-};
-
 // Message to be sent to Discord channel
-var sendMessage = '**Weekly Master Missions**\n';
+var sendMessage = "**Weekly Master Missions**\n";
 
-request(servantCall, function(error, response, html) {
+request("http://fate-go.cirnopedia.org/master_mission_us.php#nav", function(error, response, html) {
   if(!error && response.statusCode == 200) {
     //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
     var $ = cheerio.load(html);
@@ -48,11 +42,7 @@ request(servantCall, function(error, response, html) {
         i++;
       }
     }
-    //console.log(sendMessage);
     message.channel.send(`${sendMessage}`).catch(console.error);
-
-    //console.log(servant.ascensions[0].materials[0].imageUrl);
-
   }
-
+});
 }
