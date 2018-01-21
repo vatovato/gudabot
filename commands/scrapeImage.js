4044c2d83827a8a4583aa52e28a0e8b8c8bda3b4
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-21 12:30:59 -0300

    asd
    asd

--- commands/scrapeImage.js
+++ commands/scrapeImage.js
@@ -6,55 +6,50 @@ var cheerio = require('cheerio');
 const Discord = require('discord.js');
 
 
-//Servant details. Name is name, ascensions is an array with the ascension tier,
-//QP cost and materials needed for every tier
-var servant = {
-  name: '',
-  ascensionImage0: '',
-  ascensionImage1: '',
-  ascensionImage2: '',
-  ascensionImage3: ''
-};
-// Use /(icons\/servant_card\/[0-9]*.jpg)/ for regex the URL
-// Message to be sent to Discord channel
-var sendMessage = '';
+var ascensionImage0 = '';
+var ascensionImage1 = '';
+var ascensionImage2 = '';
+var ascensionImage3 = '';
+
 
 request(servantCall, function(error, response, html) {
   if(!error && response.statusCode == 200) {
     //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
     var $ = cheerio.load(html);
-    var tableWithName = $('td.desc').first();
-    var servantName = $(tableWithName).children().first().text();
-    servant.name = servantName;
-    sendMessage = "**" + servantName+ " Ascensions**\n";
 
     //Begin a switch case that scrapes the image required by imageNumber
     switch (imageNumber) {
       case "0":
       var aImage0 = $('a[title="Default Form"]').attr('href');
-      /*var divImage0 = $(aImage0).find('div').toString();
-      var image0 = divImage0.replace(/(icons\/servant_card\/[0-9]*.jpg)/g,"");*/
-      console.log(`aImage0: ${aImage0}`);
+      ascensionImage0 = "http://fate-go.cirnopedia.org" + aImage0;
+      console.log(`URL: ${ascensionImage0}`);
+      message.channel.send(`${ascensionImage0}`).catch(console.error);
       break;
 
       case "1":
-      var aImage1 = $('#1st Ascension Form').find('div').toArray().html().replace(/(icons\/servant_card\/[0-9]*.jpg)/g,"");
-      console.log(aImage1);
+      var aImage1 = $('a[title="1st Ascension Form"]').attr('href');
+      ascensionImage1 = "http://fate-go.cirnopedia.org" + aImage1;
+      console.log(`URL: ${ascensionImage1}`);
+      message.channel.send(`${ascensionImage1}`).catch(console.error);
       break;
 
       case "2":
-      var aImage2 = $('#2nd Ascension Form').find('div').html().replace(/(icons\/servant_card\/[0-9]*.jpg)/g,"");
-      console.log(aImage2);
+      var aImage2 = $('a[title="2nd Ascension Form"]').attr('href');
+      ascensionImage2 = "http://fate-go.cirnopedia.org" + aImage2;
+      console.log(`URL: ${ascensionImage2}`);
+      message.channel.send(`${ascensionImage2}`).catch(console.error);
       break;
 
       case "3":
-      var aImage3 = $('#Final Ascension Form').find('div').html().replace(/(icons\/servant_card\/[0-9]*.jpg)/g,"");
-      console.log(aImage3);
+      var aImage3 = $('a[title="Final Ascension Form"]').attr('href');
+      ascensionImage3 = "http://fate-go.cirnopedia.org" + aImage3;
+      console.log(`URL: ${ascensionImage3}`);
+      message.channel.send(`${ascensionImage3}`).catch(console.error);
       break;
     }
 
     //console.log(sendMessage);
-    //message.channel.send(`${sendMessage}`).catch(console.error);
+
   }
 });
 }
