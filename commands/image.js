37f359815bcec8578977fdd760db5d1341ca1faf
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-20 21:00:07 -0300

    asd
    asd

--- commands/image.js
+++ commands/image.js
@@ -1,22 +1,23 @@
 exports.run = (client, message, args) => {
     var servantName = args.join(" ").toLowerCase();
+    var imageNumber = servantName.slice(-1);
+    servantName = servantName.slice(0, -2);
     var urlRef = '';
     var servantUrl ='';
     var client = client;
     var message = message;
-    console.log(`Required image for ${servantName}`);
+    console.log(`Servant name: ${servantName}
+Image number: ${imageNumber}`);
 
-    console.log(`urlRef before switch: ${urlRef}`);
     var callSwitch  = require(`./switch.js`);
     urlRef = callSwitch.parseName(servantName);
-    console.log(`urlRef after switch: ${urlRef}`);
 
     if (urlRef.length == 0) {
       message.channel.send("You haven't written a valid Servant name");
     } else {
     servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef
     console.log(`URL for Servant: ${servantUrl}`);
-    let commandFile2 = require(`./scrapeImage.js`);
-    commandFile2.run(servantUrl, client, message);
+    /*let commandFile2 = require(`./scrapeImage.js`);
+    commandFile2.run(servantUrl, client, message);*/
   }
   }
