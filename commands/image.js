b80985df8cd16942368af115737e0572b22b3d3d
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-20 23:21:18 -0300

    asd
    asd

--- commands/image.js
+++ commands/image.js
@@ -8,16 +8,26 @@ exports.run = (client, message, args) => {
     var message = message;
     console.log(`Servant name: ${servantName}
 Image number: ${imageNumber}`);
+    // Checks if the number placed is a number between 1 and 4
+    if(imageNumber >= 0 && imageNumber <= 3) {
+      //Calls the switch else table with all the servant nicknames
+      // and their cirno 3-digit code
+      var callSwitch  = require(`./switch.js`);
+      urlRef = callSwitch.parseName(servantName);
 
-    var callSwitch  = require(`./switch.js`);
-    urlRef = callSwitch.parseName(servantName);
-
-    if (urlRef.length == 0) {
-      message.channel.send("You haven't written a valid Servant name");
+      // If the search in the table didn't bring any results,
+      // shows an error message.
+      if (urlRef.length == 0) {
+        message.channel.send("You haven't written a valid Servant name");
+      } else {
+        // If it did find a result, urlRef has the servant cirno code
+        // and the function can proceed to scrape the images
+        servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef
+        console.log(`URL for Servant: ${servantUrl}`);
+        let commandFile2 = require(`./scrapeImage.js`);
+        commandFile2.run(servantUrl, imageNumber, client, message);
+          }
     } else {
-    servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef
-    console.log(`URL for Servant: ${servantUrl}`);
-    let commandFile2 = require(`./scrapeImage.js`);
-    commandFile2.run(servantUrl, imageNumber, client, message);
-  }
-  }
+      message.channel.send(`There's no such thing as a level ${imageNumber} Ascension`);
+    }
+}
--- commands/scrapeImage.js
+++ commands/scrapeImage.js
@@ -5,16 +5,17 @@ var request = require('request');
 var cheerio = require('cheerio');
 const Discord = require('discord.js');
 
-//URL to bring the corresponding item from cirno
-var materialsUrl = 'http://fate-go.cirnopedia.org/icons/item/item_';
 
 //Servant details. Name is name, ascensions is an array with the ascension tier,
 //QP cost and materials needed for every tier
 var servant = {
   name: '',
-  ascensions: []
+  ascensionImage0 = '',
+  ascensionImage1 = '',
+  ascensionImage2 = '',
+  ascensionImage3 = ''
 };
-
+// Use /(icons\/servant_card\/[0-9]*.jpg)/ for regex the URL
 // Message to be sent to Discord channel
 var sendMessage = '';
 
@@ -27,68 +28,31 @@ request(servantCall, function(error, response, html) {
     servant.name = servantName;
     sendMessage = "**" + servantName+ " Ascensions**\n";
 
-    // Retrieves the div with id 'ascension' and moves to the next
-    // div that has the table with ascension details
-    var tableAscensions = $('#ascension').next();
-    // Skips the table head
-    var tableBody = $(tableAscensions).find('tbody');
-    // Retrieves all rows from the table
-    var rowsAscesions = $(tableBody).find('tr').toArray();
+    //Begin a switch case that scrapes the image required by imageNumber
+    switch (imageNumber) {
+      case "0":
+      var aImage0 = $('#Default Form').children().text();
+      console.log(aImage0);
+      break;
 
-    // For every row in the table, retrieves ascension title,
-    // QP cost of ascension and materials required. Since the materials
-    // can be more than one, they're stored in an array
-    for (let rowAscesions of rowsAscesions){
-      var ascension = {
-        title: '',
-        cost: '',
-        materials: []
-      };
-      var columnsAscensions = $(rowAscesions).find('td').toArray();
-      var i = 0;
-      for (let columnAscensions of columnsAscensions){
-        switch (i){
-          case 0:
-            ascension.title = $(columnAscensions).html().replace(/(<\/?(\s|\S)*>)/g, "");
-            sendMessage += "\n**" + ascension.title + "** ";
-            break;
-          case 1:
-            ascension.cost = $(columnAscensions).find('a').html().replace(/(<\/?(\s|\S)*>)/g, "");
-            sendMessage += "\nCost: " + ascension.cost + "\n";
-            break;
-          case 2:
-            var material = {
-              imageUrl: '',
-              materialAmount: '',
-              materialName: ''
-            }
-            var imagesMaterials = $(columnAscensions).find('a').toArray();
-            for (let imageMaterials of imagesMaterials){
-              var idImageMaterial = $(imageMaterials).attr('href').split("#")[1];
-              material.imageUrl = materialsUrl + idImageMaterial + ".png";
-              material.materialAmount = $(imageMaterials).find('div').text().replace("\n", "");
-              material.materialAmount = material.materialAmount.trim();
-              material.materialName = $(imageMaterials).attr('title').replace(/[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|\n\r/g, "");
-              material.materialName = material.materialName.trim();
-              ascension.materials.push(material);
-              sendMessage += material.materialName + " x" + material.materialAmount + " • ";
+      case "1":
+      var aImage1 = $('#1st Ascension Form').children().text();
+      console.log(aImage1);
+      break;
 
-            }
-            break;
-        }
-        i++;
-      }
-     sendMessage = sendMessage.slice(0, -2);
-     sendMessage += "\n";
-      servant.ascensions.push(ascension);
+      case "2":
+      var aImage2 = $('#2nd Ascension Form').children().text();
+      console.log(aImage2);
+      break;
 
+      case "3":
+      var aImage3 = $('#Final Ascension Form').children().text();
+      console.log(aImage3);
+      break;
     }
+
     //console.log(sendMessage);
-    sendMessage += "\nMaterials List: http://fate-go.cirnopedia.org/item.php";
-    message.channel.send(`${sendMessage}`).catch(console.error);
-
-    //console.log(servant.ascensions[0].materials[0].imageUrl);
-
+    //message.channel.send(`${sendMessage}`).catch(console.error);
   }
 });
 }
