f2ead13a4a65cf151ce5df23e485f427959fda8f
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-21 12:36:38 -0300

    asd

--- commands/scrapeImage.js
+++ commands/scrapeImage.js
@@ -21,28 +21,28 @@ request(servantCall, function(error, response, html) {
     switch (imageNumber) {
       case "0":
       var aImage0 = $('a[title="Default Form"]').attr('href');
-      ascensionImage0 = "http://fate-go.cirnopedia.org" + aImage0;
+      ascensionImage0 = "http://fate-go.cirnopedia.org/" + aImage0;
       console.log(`URL: ${ascensionImage0}`);
       message.channel.send("",{files: [ascensionImage0]}).catch(console.error);
       break;
 
       case "1":
       var aImage1 = $('a[title="1st Ascension Form"]').attr('href');
-      ascensionImage1 = "http://fate-go.cirnopedia.org" + aImage1;
+      ascensionImage1 = "http://fate-go.cirnopedia.org/" + aImage1;
       console.log(`URL: ${ascensionImage1}`);
       message.channel.send("",{files: [`${ascensionImage1}`]}).catch(console.error);
       break;
 
       case "2":
       var aImage2 = $('a[title="2nd Ascension Form"]').attr('href');
-      ascensionImage2 = "http://fate-go.cirnopedia.org" + aImage2;
+      ascensionImage2 = "http://fate-go.cirnopedia.org/" + aImage2;
       console.log(`URL: ${ascensionImage2}`);
       message.channel.send(`${ascensionImage2}`).catch(console.error);
       break;
 
       case "3":
       var aImage3 = $('a[title="Final Ascension Form"]').attr('href');
-      ascensionImage3 = "http://fate-go.cirnopedia.org" + aImage3;
+      ascensionImage3 = "http://fate-go.cirnopedia.org/" + aImage3;
       console.log(`URL: ${ascensionImage3}`);
       message.channel.send(`${ascensionImage3}`).catch(console.error);
       break;
