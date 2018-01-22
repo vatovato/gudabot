7ccc1156f6830f41090fc8f2baf74503cced4819
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-21 21:48:04 -0300

    asd

--- commands/master.js
+++ commands/master.js
@@ -13,8 +13,6 @@ request("http://fate-go.cirnopedia.org/master_mission_us.php#nav", function(erro
     var $ = cheerio.load(html);
     // Finds the first table with id="rounded-corner"
     var tableWithObjectives = $('table[id="rounded-corner"]').first();
-    var testTable = $(tableWithObjectives).find('tbody').toArray();
-    console.log(`testTable: ${testTable}`);
     // Skips table head
     var tableBody = $(tableWithObjectives).find('tbody');
     // Retrieves all rows from table
@@ -46,6 +44,44 @@ request("http://fate-go.cirnopedia.org/master_mission_us.php#nav", function(erro
     }
     sendMessage = sendMessage.replace(/&quot;/g, '\\"');
     sendMessage = sendMessage.replace(/&apos;/g, '\\´');
+    sendMessage += "\n\n**Recommended Areas**\n\n";
+
+    // Finds second table, with the Free Quests
+    var tableFreeQuests = $('table[class="height2"]');
+    var tableQBody = $(tableFreeQuests).find('tbody');
+    var rowsFree = $(tableQBody).find('tr').toArray();
+    for (let rowFree of rowsFree) {
+      var quests = {
+        quest: '',
+        ap: '',
+        target: '',
+        map: ''
+      };
+      var columnsFree = $(rowFree).find('td').toArray();
+      var j = 0;
+      for (let columnFree of columnsFree) {
+        switch(j) {
+          case 0:
+          quests.quest = $(columnFree).html().replace(/(<\/?(\s|\S)*>)/g, "");
+          quests.quest = quests.quest.replace(/[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|\n\r/g, "");
+          sendMessage += "- " + quests.quest;
+          break;
+          case 1:
+          quests.ap = $(columnFree).html().replace(/(<\/?(\s|\S)*>)/g, "");
+          sendMessage += " (" + quests.ap + ") to target ";
+          break;
+          case 3:
+          quests.target = $(columnFree).html().replace(/(<\/?(\s|\S)*>)/g, "");
+          sendMessage += quests.target + ". ";
+          break;
+          case 4:
+          quests.map = $(columnFree).html().replace(/(<\/?(\s|\S)*>)/g, "");
+          sendMessage += "Map: " + quests.map + ".\n";
+          break;
+        }
+        j++;
+      }
+    }
     message.channel.send(`${sendMessage}`).catch(console.error);
   }
 });
