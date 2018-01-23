791d1849b62fd60d61e0a398dce51b48306c4306
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-23 18:17:54 -0300

    a

--- commands/ubw.js
+++ commands/ubw.js
@@ -1,5 +1,7 @@
 exports.run = (client, message, args) => {
 var memes = ['','https://i.imgur.com/l5Wpa3t.jpg','https://i.imgur.com/3EwY2zj.jpg','https://i.imgur.com/MSM1oUj.png','https://i.imgur.com/d8Sc8SA.jpg','https://i.imgur.com/Op5fvC2.jpg'];
-
-
+var place = Math.floor((Math.random() * 5) + 1);
+var sendMeme = memes[place];
+console.log("Meme: "sendMeme);
+message.channel.send("",{files: [sendMeme]});
 }
