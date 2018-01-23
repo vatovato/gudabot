7e6f12dbbc12ec380286f2adde66bca6936c2564
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-23 11:28:59 -0300

    asd

--- commands/scrapeServant.js
+++ commands/scrapeServant.js
@@ -26,15 +26,19 @@ request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, res
         case 3:
         servant.name = $(tableColumns).find('a').text();
         servant.name = servant.name.replace(/[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|\n\r/g, "");
-        console.log(servant.name);
           break;
       }
       i++;
     }
     servant.image = "http://fate-go.cirnopedia.org/icons/servant/servant_"+urlRef+"1.png";
-    message.channel.send(`Name: ${servant.name}
-Image: ${servant.image}`);
-  }
+    //message.channel.send(`Name: ${servant.name}
+//Image: ${servant.image}`);
+    const embed = new Discord.RichEmbed()
+    .setTitle(servant.name)
+    .setThumbnail(servant.image)
+    .setURL(servantCall)
+    message.channel.send({embed});
+    }
 
   });
 }
