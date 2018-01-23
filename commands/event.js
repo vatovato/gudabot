0868f9a5b9b821cb3a6d3f85183799101a3c40b0
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-23 09:25:48 -0300

    asd
    ad

--- commands/event.js
+++ commands/event.js
@@ -1,4 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`Current event: http://fategrandorder.wikia.com/wiki/2M_Download_Campaign_(US)
-Future event: http://fate-go.us/news/?category=NEWS&article=%2Fiframe%2F2018%2F0119_valentines%2F`).catch(console.error);
+    message.channel.send(`Current event - 2 Million Download Campaign: http://fategrandorder.wikia.com/wiki/2M_Download_Campaign_(US)
+    Future event - Valentine's 2018: https://webview.fate-go.us/iframe/2018/0123_valentines/`).catch(console.error);
 }
--- commands/servant.js
+++ commands/servant.js
@@ -1,16 +1,21 @@
 
 exports.run = (client, message, args) => {
+
+  var request = require('request');
+  var cheerio = require('cheerio');
+  const Discord = require('discord.js');
 //Concatenates all args to form the servant name called for the function
-    var servantName = args.join(" ").toLowerCase();
-    var invalidServantName = args.join("+").toLowerCase();
-    var searchUrl = "https://www.google.com.ar/search?q=" + invalidServantName + "+site%3Ahttp%3A%2F%2Ffate-go.cirnopedia.org";
-    //Stores the 3 digit code that identifies a Servant in cirno
-    var urlRef = '';
-    //URL for the servant called in servantName
-    var servantUrl ='';
-    var client = client;
-    var message = message;
-    console.log(`Required cirno page for for ${servantName}`);
+  var servantName = args.join(" ").toLowerCase();
+  var invalidServantName = args.join("+").toLowerCase();
+  var searchUrl = "https://www.google.com.ar/search?q=" + invalidServantName + "+site%3Ahttp%3A%2F%2Ffate-go.cirnopedia.org";
+  //Stores the 3 digit code that identifies a Servant in cirno
+  var urlRef = '';
+  //URL for the servant called in servantName
+  var servantUrl ='';
+  var client = client;
+  var message = message;
+  var name = '';
+  console.log(`Required cirno page for for ${servantName}`);
 
     var callSwitch  = require(`./switch.js`);
     urlRef = callSwitch.parseName(servantName);
@@ -20,14 +25,16 @@ exports.run = (client, message, args) => {
     } else {
     //message.channel.send('Link to Cirno: http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef);
     servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef;
-    
-    message.channel.send(`${servantUrl}`);
-    /* let commandFile2 = require(`./scrapeServant.js`);
-    commandFile2.run(urlRef, servantUrl, client, message); */
+    request(servantUrl, function(error, response, html) {
+      if(!error && response.statusCode == 200) {
+        //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
+        var $ = cheerio.load(html);
+        var tableWithName = $('td.desc').first();
+        name = $(tableWithName).children().first().text();
+      }
+    });
+
+    message.channel.send(`URL: ${servantUrl}
+Name: ${name}`);
   }
-    /* let age = args[0];
-    let sex = args[1];
-    let location = args[2];
-    message.channel.send(`Hello, ${message.author.username}, I see you're a ${age} old ${sex} from ${location}. Wanna date?`).catch(console.error);
-*/
 }
