0d2f8999d791407cbb35740b4a790ae26cd77bb4
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-23 11:03:56 -0300

    asd

--- commands/scrapeServant.js
+++ commands/scrapeServant.js
@@ -4,15 +4,32 @@ var urlRef = urlRef;
 var request = require('request');
 var cheerio = require('cheerio');
 const Discord = require('discord.js');
-var serName = '';
+var servant = {
+  name: '';
+  image: ''
+};
 
 // Retrieves Servant Name from Servant specific URL
 request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, response, html) {
   if(!error && response.statusCode == 200) {
     //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
     var $ = cheerio.load(html);
-    var fullTable = $('tr[id="'+urlRef+'"]').toArray();
-    console.log(fullTable);
+    var fullTable = $('tr[id="'+urlRef+'"]');
+    var tablesColumns = $(fullTable).find('td').toArray();
+    var i = 0;
+    for (let tableColumns of tablesColumns){
+      switch (i){
+        case 2:
+        servant.image = $(tableColumns).find('div').attr('style');
+        console.log(servant.image);
+          break;
+        case 3:
+        servant.name = $(tableColumns).find('a').text();
+        console.log(servant.name);
+          break;
+      }
+      i++;
+    }
     }
   });
 
@@ -25,5 +42,4 @@ request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, res
     console.log(tableA);
   }
 });*/
-message.channel.send(`${serName}: ${servantCall}`);
 }
