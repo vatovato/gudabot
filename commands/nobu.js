e790233aa622f5a138aee581ff4094e51c881833
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-23 17:41:36 -0300

    a

--- commands/nobu.js
+++ commands/nobu.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
-    const badCiv = client.emojis.get(380227835014414336);
+    //const badCiv = client.emojis.get(380227835014414336);
+    const umuEmoji = client.emojis.get(401386848796016650);
     message.channel.send("No !nobu command until you give me a nice video!").catch(console.error);
-    message.channel.send(badCiv).catch(console.error);
+    message.channel.send(umuEmoji).catch(console.error);
 }
