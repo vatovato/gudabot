af36bccf84634df5bdd81b7d350a142da3cb3b49
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-23 12:22:25 -0300

    asd

--- commands/scrapeServant.js
+++ commands/scrapeServant.js
@@ -59,13 +59,13 @@ request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, res
           console.log("Card: " + card);
           var cardCheck = $(card).attr('src');
           console.log("cardCheck: " + cardCheck);
-          /*if(cardCheck.indexOf("pattern_01") >= 0) {
+          if(cardCheck.indexOf("pattern_01") >= 0) {
             servant.cards += "Quick";
           } else if (cardCheck.indexOf("pattern_02") >= 0) {
             servant.cards += "Arts";
           } else if (cardCheck.indexOf("pattern_03") >= 0) {
             servant.cards += "Buster";
-          }*/
+          }
             servant.cards += " ";
             console.log("Servant Cards: " + servant.cards);
           }
