9c11bfccd3b0b1e73ca2d15163e9a24bb42fbc1a
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-24 12:50:14 -0300

    a

---
+++ commands/whale.js
@@ -0,0 +1,13 @@
+exports.run = (client, message, args) => {
+    message.channel.send(`**whale tier**
+ustnap, rephin, boat, kayos90, grimmjow, jonjonaug
+
+**dolphin tier**
+randomless, neki, lavenza, karnapolice, einbroch, david, sabrekins, grazzt, baekshi, remembrance, unknown, t-rex, shinogu, azusa
+
+**f2p**
+haly, laiza, arti, sean, hubb, gryffinp, furiosa, mupod, jeff chickendig, strifeser, sabin (pretends to be), zanzura (memes)
+
+**ex rank luck**
+remembrance, saberkins, xia`).catch(console.error);
+}
