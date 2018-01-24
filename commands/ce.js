b2a550f41c5d4aa259df76d35d1f834c4b9c5023
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-24 16:52:27 -0300

    as

---
+++ commands/ce.js
@@ -0,0 +1,6 @@
+exports.run = (client, message, args) => {
+    message.channel.send(`No. Hell no. I'm not touching that table, you can go look yourelf. Here, go nuts:
+Cirno: http://fate-go.cirnopedia.org/craft_essence.php#nav
+Wikia: http://fategrandorder.wikia.com/wiki/Craft_Essence_List
+Wiki: https://grandorder.wiki/Craft_Essences`).catch(console.error);
+}
