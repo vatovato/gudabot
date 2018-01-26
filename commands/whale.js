dea634b281419e839cab96f216a5458768a17f6d
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-01-26 09:11:26 -0300

    a

--- commands/whale.js
+++ commands/whale.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
     message.channel.send(`**whale tier**
-ustnap, rephin, boat, kayos90, grimmjow, jonjonaug
+ustnap, rephin, boat, kayos90, grimmjow, jonjonaug, dark494, randomdude
 
 **dolphin tier**
 randomless, neki, lavenza, karnapolice, einbroch, david, sabrekins, grazzt, baekshi, remembrance, unknown, t-rex, shinogu, azusa
 
 **f2p**
-haly, laiza, arti, sean, hubb, gryffinp, furiosa, mupod, jeff chickendig, strifeser, sabin (pretends to be), zanzura (memes)
+haly, laiza, arti, sean, hubb, gryffinp, furiosa, mupod, jeff chickendig, strifeser, sabin (pretends to be), zanzura, ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou, nightengale, sadness enchancement ex, dandte, saci
 
 **ex rank luck**
-remembrance, saberkins, xia`).catch(console.error);
+remembrance, saberkins, xia, ironraven`).catch(console.error);
 }
