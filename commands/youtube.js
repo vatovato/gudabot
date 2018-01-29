d743ecd3839edbd62e81b041ca86d9accac4b391
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-28 23:34:16 -0300

    YouTube command

---
+++ commands/youtube.js
@@ -0,0 +1,13 @@
+
+exports.run = (client, message, args) => {
+
+
+  var youTubeQuery = args.join("+").toLowerCase();
+  var searchUrl = "https://www.youtube.com/results?search_query=" + youTubeQuery;
+  //Stores the 3 digit code that identifies a Servant in cirno
+  var client = client;
+  var message = message;
+  console.log(`Required youtube search for ${youTubeQuery}`);
+
+      message.channel.send(`Search for ${youTubeQuery}: ${searchUrl}`);
+}
