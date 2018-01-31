efe73b61627b8c49a0d1df7936e394f4b3a40fda
eugenioteran <eugenioteran@users.noreply.github.com>
2018-01-30 22:06:04 -0300

    a

--- commands/umu.js
+++ commands/umu.js
@@ -4,7 +4,9 @@ const client = new Discord.Client();
 exports.run = (client, message, args) => {
     /* const umu = client.emojis.get("401386848796016650");
     message.reply(`${umu}`); */
-
+var umuType = args.join(" ").toLowerCase();
+console.log(umuType);
+if(umuType == "rojo" || umuType == "") {
     const embed = new Discord.RichEmbed()
         .setTitle("UMU","umu")
         .setImage("https://cdn.discordapp.com/emojis/401386848796016650.png")
@@ -13,4 +15,23 @@ exports.run = (client, message, args) => {
         .addField("umu","umu",true)
 
     message.channel.send({ embed });
+  } else if (umuType == "blanco") {
+    const embed = new Discord.RichEmbed()
+        .setTitle("UMU","umu")
+        .setImage("https://i.imgur.com/Z8amJs7.png")
+        .setDescription("umu")
+        .setFooter("umu")
+        .addField("umu","umu",true)
+
+    message.channel.send({ embed });
+  } else if (umuType == "verano") {
+    const embed = new Discord.RichEmbed()
+        .setTitle("UMU","umu")
+        .setImage("https://i.imgur.com/3wXCF7X.jpg")
+        .setDescription("umu")
+        .setFooter("umu")
+        .addField("umu","umu",true)
+
+    message.channel.send({ embed });
+  }
 };
---
+++ commands/umu_OLD.js
@@ -0,0 +1,27 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+exports.run = (client, message, args) => {
+    /* const umu = client.emojis.get("401386848796016650");
+    message.reply(`${umu}`); */
+var umuType = args.join(" ").toLowerCase();
+
+if(umuType == "rojo" or "") {
+    const embed = new Discord.RichEmbed()
+        .setTitle("UMU","umu")
+        .setImage("https://cdn.discordapp.com/emojis/401386848796016650.png")
+        .setDescription("umu")
+        .setFooter("umu")
+        .addField("umu","umu",true)
+
+    message.channel.send({ embed });
+  } else if (umuType == "blanco") {
+    const embedBlanco = new Discord.RichEmbed()
+    .setTitle("UMU","umu")
+    .setImage("https://i.imgur.com/Z8amJs7.png")
+    .setDescription("umu")
+    .setFooter("umu")
+    .addField("umu","umu",true)
+    message.channel.send({embedBlanco});
+  }
+};
