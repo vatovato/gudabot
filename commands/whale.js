8f153aadb28c8e43defaf2f02dba2785c784bea9
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-02 16:02:59 -0300

    a

--- commands/whale.js
+++ commands/whale.js
@@ -1,13 +1,13 @@
 exports.run = (client, message, args) => {
     message.channel.send(`**Whale Tier**
-ustnap, rephin, boat, kayos90, neki, grimmjow, jonjonaug, dark494, randomdude
+ustnap, rephin, boat, kayos90, neki, grimmjow, jonjonaug, dark494, randomdude, safflower
 
 **Dolphin Tier**
-randomless, lavenza, karnapolice, einbroch, david, sabrekins, grazzt, baekshi, remembrance, unknown, t-rex, shinogu, azusa, mupod, pureauthor, dandte, safflower, enronjp
+randomless, lavenza, karnapolice, einbroch, david, sabrekins, grazzt, baekshi, remembrance, unknown, t-rex, shinogu, azusa, mupod, pureauthor, dandte, enronjp, zanzura
 
 **F2P**
-haly, laiza, arti, sean, hubb, gryffinp, furiosa, jeff chickendig, strifeser, sabin (pretends to be), zanzura, ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou, nightengale, sadness enchancement ex, saci
+haly, laiza, arti, sean, hubb, gryffinp, furiosa, jeff chickendig, strifeser, sabin (pretends to be), ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou, nightengale, sadness enchancement ex, saci
 
 **EX Rank Luck**
-remembrance, saberkins, xianaphryz, ironraven`).catch(console.error);
+remembrance, saberkins, xianaphryz, ironraven, hentailover`).catch(console.error);
 }
