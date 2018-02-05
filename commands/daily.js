50ee03a8548c155bd1110b64c1674219f37d1411
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-05 20:38:38 -0300

    a

---
+++ commands/daily.js
@@ -0,0 +1,11 @@
+exports.run = (client, message, args) => {
+
+message.channel.send(`**Monday** - Archer Training Ground / Lancer & Assassin EXP Cards.
+**Tuesday** - Lancer Training Ground / Saber & Rider EXP Cards.
+**Wednesday** - Berserker Training Ground / Archer & Caster EXP Cards.
+**Thursday** - Rider Training Ground / Lancer & Assassin EXP Cards.
+**Friday** - Caster Training Ground / Saber & Rider EXP Cards.
+**Saturday** - Assassin Training Ground / Archer & Caster EXP Cards.
+**Sunday** - Saber Training Ground / All Classes EXP Cards.`);
+
+}
