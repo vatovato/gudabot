fcfcb897f0348c46857c35344a3e425b8f55d1f7
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-06 15:23:54 -0300

    a

--- commands/addfc.js
+++ commands/addfc.js
@@ -7,12 +7,12 @@ var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
+
 if(args.length == 0) {
   message.channel.send("You did not add your Friend Code. Use !addfc friend_code. For example, !addfc 123,123,123.");
 } else {
   console.log(authorName + " wants to add / update his friend code.");
   connection.query(`SELECT * FROM friends WHERE userId ="${authorId}"`, function(err, rows, fields) {
-    if (err) throw err;
     if(!rows) {
       console.log("User did not exist. Creating.");
       connection.query(`INSERT INTO friends (userId, friendCode) VALUES (${authorId}, ${fc})`);
