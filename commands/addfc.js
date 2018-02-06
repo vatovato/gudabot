840bb338bb3869b16dfd7ae9902e6a17933f371d
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-06 15:08:40 -0300

    a

--- commands/addfc.js
+++ commands/addfc.js
@@ -1,3 +1,41 @@
 exports.run = (client, message, args) => {
-message.channel.send("Sorry, turns out persistence isn't free in Heroku so no automatic friend code adding and calling.");
+//message.channel.send("Sorry, turns out persistence isn't free in Heroku so no automatic friend code adding and calling.");
+var authorId = message.author.id;
+var authorName = message.author.username;
+var fc = args.join(" ");
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+if(args.length == 0) {
+  message.channel.send("You did not add your Friend Code. Use !addfc friend_code. For example, !addfc 123,123,123.");
+} else {
+  console.log(authorName + " wants to add / update his friend code.");
+  connection.query(`SELECT * FROM friends WHERE userId ="${authorId}"`, function(err, rows, fields) {
+    if (err) {
+      console.log(err);
+      connection.query("CREATE TABLE IF NOT EXISTS friends (userId TEXT, friendCode TEXT)");
+      console.log("Table did not exist, creating.");
+      connection.query(`INSERT INTO friends (userId, friendCode) VALUES (${authorId}, ${fc})`);
+      message.channel.send(`Table did not exist. Created table and added **${fc}** to User **${authorName}**.`);
+    }
+    if(!rows) {
+      console.log("User did not exist. Creating.");
+      connection.query(`INSERT INTO friends (userId, friendCode) VALUES (${authorId}, ${fc})`);
+      message.channel.send(`**${authorName}**, you have created your Friend Code as **${fc}**.`);
+    } else {
+      console.log("User already exists. Updating.");
+      connection.query(`UPDATE friends SET friendCode = "${fc}" WHERE userId = "${authorId}"`);
+      message.channel.send(`**${authorName}**, you have updated your Friend Code to **${fc}**.`);
+    }
+  });
+}
+/*connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});*/
+
+connection.end();
+
 }
