1ef2d73bf04e839be7d2ae8085a5cf6320a76d69
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-06 16:00:33 -0300

    a

--- commands/addfc.js
+++ commands/addfc.js
@@ -13,15 +13,15 @@ if(args.length == 0) {
   message.channel.send("You did not add your Friend Code. Use !addfc friend_code. For example, !addfc 123,123,123.");
 } else {
   console.log(authorName + " wants to add / update his friend code.");
-  connection.query(`SELECT * FROM friends WHERE userId ="${authorId}"`, function(err, rows, fields) {
+  connection.query(`SELECT * FROM friends WHERE userId ='${authorId}'`, function(err, rows, fields) {
     if (err) throw err;
     if(rows.length == 0) {
       console.log("User did not exist. Creating.");
-      connection.query(`INSERT INTO friends SET 'userId' = '${authorId}', 'friendCode' = '${fc}')`);
+      connection.query(`INSERT INTO friends SET userId = '${authorId}', friendCode = '${fc}')`);
       message.channel.send(`**${authorName}**, you have created your Friend Code as **${fc}**.`);
     } else {
       console.log("User already exists. Updating.");
-      connection.query(`UPDATE friends SET 'friendCode' = '${fc}' WHERE 'userId' = '${authorId}'`);
+      connection.query(`UPDATE friends SET friendCode = '${fc}' WHERE userId = '${authorId}'`);
       message.channel.send(`**${authorName}**, you have updated your Friend Code to **${fc}**.`);
     }
   });
