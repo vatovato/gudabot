399d26b7802898c451afef4d4a3eb38845cdd10a
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-09 12:56:32 -0300

    a

--- commands/event.js
+++ commands/event.js
@@ -1,3 +1,6 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`No current event. Next is Kara no Kyoukai: http://fate-go.cirnopedia.org/quest_event_20160225.php#nav`).catch(console.error);
+    message.channel.send(`**The Garden of Sinners - Garden of Order**
+Cirno: http://fate-go.cirnopedia.org/quest_event_20160225.php#nav
+Wiki: https://grandorder.wiki/The_Garden_of_sinners_-_the_Garden_of_Order
+Wikia: http://fategrandorder.wikia.com/wiki/The_Garden_of_Sinners_Collaboration_Event_(US)`).catch(console.error);
 }
