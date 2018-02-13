47e840a940cf58c03e4cf58a8ba7defbd84350de
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-13 14:27:51 -0300

    a

--- commands/event.js
+++ commands/event.js
@@ -2,5 +2,6 @@ exports.run = (client, message, args) => {
     message.channel.send(`**The Garden of Sinners - Garden of Order**
 Cirno: <http://fate-go.cirnopedia.org/quest_event_20160225.php#nav>
 Wiki: <https://grandorder.wiki/The_Garden_of_sinners_-_the_Garden_of_Order>
-Wikia: <http://fategrandorder.wikia.com/wiki/The_Garden_of_Sinners_Collaboration_Event_(US)>`).catch(console.error);
+Wikia: <http://fategrandorder.wikia.com/wiki/The_Garden_of_Sinners_Collaboration_Event_(US)>
+Next event is **Prison Tower's Howling Demon of Vengeance**: http://fate-go.cirnopedia.org/quest_event_20160316.php#nav`).catch(console.error);
 }
