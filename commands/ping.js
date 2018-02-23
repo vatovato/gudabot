74fba987319dd913da83373151923141e2e90930
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-23 18:12:59 -0300

    a

---
+++ commands/ping.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args) => {
+
+var args = args.toString().toLowerCase();
+if(args === "kayos" || args === "kayos90") {
+  message.channel.send("A-are you insane? I don't wanna be deleted <:blobsweats:365262148814766080>");
+}
+
+}
