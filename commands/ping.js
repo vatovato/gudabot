4067a55092e0f9b3baa3e7ee667975abf91538c3
eugenioteran <eugenioteran@users.noreply.github.com>
2018-02-23 18:28:14 -0300

    a

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 exports.run = (client, message, args) => {
 
+var authorName = message.author.username;
 var args = args.toString().toLowerCase();
 if(args === "kayos" || args === "kayos90") {
   message.channel.send("A-are you insane? I don't wanna be deleted <:blobsweats:365262148814766080>");
+  console.log(authorName + " tried to ping kayos!!!");
 }
 
 }
---
+++ commands/washija.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("",{files: ["https://i.imgur.com/GYpQhKB.png"]}).catch(console.error);
+}
