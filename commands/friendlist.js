2bb6b8ea96b8ebbed74e72d91f8ac33388dc6637
eugenioteran <eugenioteran@users.noreply.github.com>
2018-03-10 17:17:23 -0300

    a

---
+++ commands/friendlist.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("https://docs.google.com/spreadsheets/d/1nVNXJ8J8rbaOodw4Masx_19mfYuKE481hA-v2L0uB8c/edit#gid=0").catch(console.error);
+}
