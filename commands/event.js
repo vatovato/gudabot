cb44ddf9808c5e0ef18b88a977ba09067e83f1ab
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-03-15 10:52:58 -0300

    a

--- commands/event.js
+++ commands/event.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`No event for now, go have fun with 1/2 AP Training Grounds: https://webview.fate-go.us/iframe/2018/0307_training_ap_half/`).catch(console.error);
+    message.channel.send(`No event for now, go finish America: https://webview.fate-go.us/2018/0313_chapter5/`).catch(console.error);
 }
