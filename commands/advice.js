ac80bbfdcaa78436166072e08d4bbdd974cdc047
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-03-16 15:28:02 -0300

    !advice

---
+++ commands/advice.js
@@ -0,0 +1,7 @@
+exports.run = (client, message, args) => {
+var advice = ['', 'https://i.imgur.com/e1Hzivz.png', 'https://i.imgur.com/ylsQI8b.jpg', 'https://i.imgur.com/ss1J7I8.jpg', 'https://i.imgur.com/lkqwAS1.jpg', 'https://i.imgur.com/fQ5yHnV.png', 'https://i.imgur.com/VLVPEhH.jpg', 'https://i.imgur.com/EuW84zA.png', 'https://i.imgur.com/ASbCjOa.png'];
+var place = Math.floor((Math.random() * 8) + 1);
+var sendAdvice = advice[place];
+console.log("Meme: " + sendAdvice);
+message.channel.send("",{files: [sendAdvice]});
+}
