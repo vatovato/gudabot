de1b355d5b3b4880546e92374a2d232f44780741
eugenioteran <eugenioteran@users.noreply.github.com>
2018-03-18 12:56:15 -0300

    advice

--- commands/advice.js
+++ commands/advice.js
@@ -1,6 +1,26 @@
 exports.run = (client, message, args) => {
-var advice = ['', 'https://i.imgur.com/e1Hzivz.png', 'https://i.imgur.com/ylsQI8b.jpg', 'https://i.imgur.com/ss1J7I8.jpg', 'https://i.imgur.com/lkqwAS1.jpg', 'https://i.imgur.com/fQ5yHnV.png', 'https://i.imgur.com/VLVPEhH.jpg', 'https://i.imgur.com/EuW84zA.png', 'https://i.imgur.com/ASbCjOa.png', 'https://i.imgur.com/CX8r7Ws.png'];
-var place = Math.floor((Math.random() * 9) + 1);
+
+if(message.channel.name == "nsfw") {
+  message.channel.send("",{files: ["https://i.imgur.com/4W9g2nC.png"]});
+  return;
+}
+
+var advice = ['',
+'https://i.imgur.com/tkJEQae.jpg',
+'https://i.imgur.com/1Cox3ja.jpg',
+'https://i.imgur.com/Hp9cSpG.jpg',
+'https://i.imgur.com/j4wzBxq.jpg',
+'https://i.imgur.com/E89IPDy.jpg',
+'https://i.imgur.com/4nl0Dtx.jpg',
+'https://i.imgur.com/0xXu3FI.jpg',
+'https://i.imgur.com/Jd2Xs9A.jpg',
+'https://i.imgur.com/dubY4Gg.jpg',
+'https://i.imgur.com/G6SDUg1.jpg',
+'https://i.imgur.com/47zI3oK.jpg',
+'https://i.imgur.com/ONlSgKw.jpg',
+'https://i.imgur.com/oFusFpN.jpg',
+'https://i.imgur.com/2h2YOhy.jpg'];
+var place = Math.floor((Math.random() * 14) + 1);
 var sendAdvice = advice[place];
 console.log("Meme: " + sendAdvice);
 message.channel.send("",{files: [sendAdvice]});
