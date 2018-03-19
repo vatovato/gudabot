1326af9aab1a58d575af659a3d7ab876a70a7ea7
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-03-19 11:07:18 -0300

    daishouri

---
+++ commands/daishouri.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("https://cdn.discordapp.com/attachments/332285823300927500/425293666878226432/image.gif").catch(console.error);
+}
