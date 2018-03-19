300cbcba091c741ef3e2f8159808f768531c81cd
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-03-19 13:55:24 -0300

    a

--- commands/jannu.js
+++ commands/jannu.js
@@ -3,6 +3,9 @@ var args = args.toString().toLowerCase();
 if(args == "alter") {
   message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310765734690828/jannu_alter.gif").catch(console.error);
   return;
+} else if (args == "fast") {
+  message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425335640243044362/JANNUUUU.gif").catch(console.error);
+  return;
 }
 message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310265844695040/jannu.gif").catch(console.error);
 }
