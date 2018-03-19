a941479d0c08684de73cf63aea118bfb9470f236
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-03-19 12:35:21 -0300

    jannu

---
+++ commands/jannu.js
@@ -0,0 +1,8 @@
+exports.run = (client, message, args) => {
+var args = args.toString().toLowerCase();
+if(args == "alter") {
+  message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310765734690828/jannu_alter.gif").catch(console.error);
+  return;
+}
+message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310265844695040/jannu.gif").catch(console.error);
+}
