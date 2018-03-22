bd955532db403810b49119e876588a3fe19896a5
eugenioteran <eugenioteran@users.noreply.github.com>
2018-03-22 20:58:05 -0300

    a

--- commands/addwl.js
+++ commands/addwl.js
@@ -10,27 +10,34 @@ var nickname = message.member.nickname;
 var wishlist = args.toString().replace(/,/g, " ");
 var defaultImage = "https://i.imgur.com/bqd5u1r.png";
 
+if(nickname === null) {
 
+console.log(username + " wants to add / update his wishlist.");
+connection.query(`SELECT * FROM wishlist WHERE userID = '${userID}'`, function(err, rows, fields) {
+  if(err) throw err;
+  if (rows.length == 0) {
+    console.log("User did not exist. Creating.");
+    connection.query(`INSERT INTO wishlist SET userID = '${userID}', username = '${username}', nickname = '${nickname}', wishlist = '${wishlist}', imageURL = '${defaultImage}'`);
+    message.channel.send(`**${username}**, you have created your wishlist as **${wishlist}**`);
+  } else {
+    console.log("User already exists. Updating.");
+    connection.query(`UPDATE wishlist SET wishlist = '${wishlist}', username = '${username}' WHERE userID = '${userID}'`);
+  }
+});
+
+} else {
   console.log(nickname + " wants to add / update his wishlist.");
   connection.query(`SELECT * FROM wishlist WHERE userID ='${userID}'`, function(err, rows, fields) {
     if (err) throw err;
     if(rows.length == 0) {
       console.log("User did not exist. Creating.");
       connection.query(`INSERT INTO wishlist SET userID = '${userID}', username = '${username}', nickname = '${nickname}', wishlist = '${wishlist}', imageURL = '${defaultImage}'`);
-          if(nickname === null) {
-          message.channel.send(`**${username}**, you have created your wishlist as **${wishlist}**.`);
-        } else {
-          message.channel.send(`**${nickname}**, you have created your wishlist as **${wishlist}**.`);
-        }
+      message.channel.send(`**${nickname}**, you have created your wishlist as **${wishlist}**.`);
     } else {
       console.log("User already exists. Updating.");
-      connection.query(`UPDATE wishlist SET wishlist = '${wishlist}'  WHERE userID = '${userID}'`);
-          if(nickname === null) {
-          message.channel.send(`**${username}**, you have updated your wishlist to **${wishlist}**.`);
-        } else {
-          message.channel.send(`**${nickname}**, you have updated your wishlist to **${wishlist}**.`);
-        }
+      connection.query(`UPDATE wishlist SET wishlist = '${wishlist}', username = '${username}', nickname = '${nickname}'  WHERE userID = '${userID}'`);
+      message.channel.send(`**${nickname}**, you have updated your wishlist to **${wishlist}**.`);
     }
   });
-
+ }
 }
--- commands/wl.js
+++ commands/wl.js
@@ -4,54 +4,63 @@ var userID = message.author.id;
 var username = message.author.username;
 var nickname = message.member.nickname;
 var wishowner = '';
+const Discord = require('discord.js');
+
 console.log("args: " + args);
 //if the user only calls !wishlist, it assumes it's asking for its own wishlist. Otherwise it looks for the args in the db
 if(args == "") {
-  if(nickname === null) {
-    wishowner = username;
-  } else {
-  wishowner = nickname;
-}
-} else {
-
-  wishowner = args;
-}
-
-console.log("wishowner: " + wishowner);
-const Discord = require('discord.js');
-
-console.log(username + " wants to call " + wishowner + "'s wishlist.");
-
-if(nickname === null) {
-connection.query(`SELECT * FROM wishlist WHERE username LIKE '%${wishowner}%'`, function(err, rows, fields) {
-  if (err) throw err;
+console.log(username + " (" + nickname + ") wants to call his wishlist.");
+connection.query(`SELECT * FROM wishlist WHERE userID = '${userID}'`, function(err, rows, fields) {
+  if(err) throw err;
   if(rows.length == 0) {
-    console.log("User did not exist.");
-    message.channel.send(`**${username}**, the name you entered isn't even remotely similar to anything in the database. What the hell...`);
+    message.channel.send(`${username} you're not on the list yet. Use !addwl [wishlist] first.`);
   } else {
     var userUsername = rows[0].username;
+    var userNickname = rows[0].nickname;
     var userWishlist = rows[0].wishlist;
+    var embedName = '';
+      if(userNickname === null) {
+        embedName = userUsername;
+      } else {
+        embedName = userNickname;
+      }
     var image = rows[0].imageURL;
     const embed = new Discord.RichEmbed()
-    .setTitle(`${userUsername}'s Wishlist`)
+    .setTitle(`${embedName}'s Wishlist`)
     .setThumbnail(image)
-    .setDescription(`${userWishlist}`)
+    .setDescription(userwishlist)
     message.channel.send({embed});
   }
 });
+
 }
-else {
-  connection.query(`SELECT * FROM wishlist WHERE nickname LIKE '%${wishowner}%'`, function(err, rows, fields) {
+ else {
+  wishowner = args.toString().replace(/,/g, " ");
+  console.log(username + " wants to call " + wishowner + "'s wishlist.");
+  connection.query(`SELECT * FROM wishlist WHERE username LIKE '%${wishowner}%'`, function(err, rows, fields) {
     if (err) throw err;
     if(rows.length == 0) {
-      console.log("User did not exist.");
-      message.channel.send(`**${nickname}**, the name you entered isn't even remotely similar to anything in the database. What the hell...`);
+      connection.query(`SELECT * FROM wishlist WHERE nickname LIKE '%${wishowner}%'`, function(err2, rows2, fields2) {
+        if (err2) throw err2;
+        if(rows2.length == 0) {
+          message.channel.send(`I can't find anybody with that name in the database.`);
+        } else {
+          var userNickname = rows2[0].nickname;
+          var userWishlist = rows2[0].wishlist;
+          var image = rows2[0].imageURL;
+          const embed = new Discord.RichEmbed()
+          .setTitle(`${userNickname}'s Wishlist`)
+          .setThumbnail(image)
+          .setDescription(userWishlist)
+          message.channel.send({embed});
+        }
+      });
     } else {
-      var userNickname = rows[0].nickname;
+      var userUsername = rows[0].username;
       var userWishlist = rows[0].wishlist;
       var image = rows[0].imageURL;
       const embed = new Discord.RichEmbed()
-      .setTitle(`${userNickname}'s Wishlist`)
+      .setTitle(`${userUsername}'s Wishlist`)
       .setThumbnail(image)
       .setDescription(`${userWishlist}`)
       message.channel.send({embed});
@@ -59,19 +68,3 @@ else {
   });
 }
 }
-/*connection.query(`SELECT * FROM wishlist WHERE username LIKE '%${wishowner}%'`, function(err, rows, fields) {
-  if (err) throw err;
-  if(rows.length == 0) {
-    console.log("User did not exist.");
-    message.channel.send(`**${username}**, the name you entered isn't even remotely similar to anything in the database. What the hell...`);
-  } else {
-    var userUsername = rows[0].username;
-    var userWishlist = rows[0].wishlist;
-    var image = rows[0].imageURL;
-    const embed = new Discord.RichEmbed()
-    .setTitle(`${userUsername}'s Wishlist`)
-    .setThumbnail(image)
-    .setDescription(`${userWishlist}`)
-    message.channel.send({embed});
-  }
-});*/
