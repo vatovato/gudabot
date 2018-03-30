fef0b0a4f6841bb08de9cd7aa806bfd60071989b
eugenioteran <eugenioteran@users.noreply.github.com>
2018-03-30 14:47:05 -0300

    a

--- commands/whale.js
+++ commands/whale.js
@@ -6,8 +6,8 @@ ustnap, rephin, boat, kayos90, neki, grimmjow, jonjonaug, dark494, randomdude, s
 randomless, lavenza, karnapolice, einbroch, david, sabrekins, grazzt, baekshi, remembrance, unknown, t-rex, shinogu, azusa, mupod, dandte, enronjp, zanzura
 
 **F2P**
-haly, laiza, arti, sean, gryffinp, furiosa, jeff chickendig, strifeser, sabin (pretends to be), ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou, nightengale, sadness enchancement ex, saci, zerokool, line x
+haly, laiza, arti, sean, furiosa, jeff chickendig, strifeser, sabin (pretends to be), ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou, nightengale, sadness enchancement ex, saci, zerokool, line x
 
 **EX Rank Luck**
-remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom`).catch(console.error);
+remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom, gryffinp, sirkenai`).catch(console.error);
 }
