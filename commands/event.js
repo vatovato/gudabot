18d73172d440a85576f76d1a1e70e5c5a80fdb87
eugenioteran <eugenioteran@users.noreply.github.com>
2018-03-31 18:49:57 -0300

    a

--- commands/event.js
+++ commands/event.js
@@ -1,3 +1,3 @@
 exports.run = (client, message, args) => {
-    message.channel.send(`No event for now, go finish America: https://webview.fate-go.us/2018/0313_chapter5/`).catch(console.error);
+    message.channel.send(`FINALLY JALTER http://fate-go.us/event/`).catch(console.error);
 }
