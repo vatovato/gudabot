86e5112da99940497e7feebcf8a7bfc05bb5ed0c
eugenioteran <eugenioteran@users.noreply.github.com>
2018-06-16 12:02:29 -0300

    a

--- commands/whale.js
+++ commands/whale.js
@@ -3,17 +3,17 @@ exports.run = (client, message, args) => {
 rephin
 
 **Whale Tier**
-ustnap, boat, kayos90, neki (multi-gacha whale), grimmjow, jonjonaug, dark494, randomdude, safflower, pureauthor, t-rex (spook king)
+ustnap, boat, kayos90, neki (multi-gacha whale), grimmjow, jonjonaug, dark494, randomdude, safflower, pureauthor, t-rex (spook king), unknown, enronjp
 
 **Dolphin Tier**
-randomless, karnapolice, einbroch, sabrekins, baekshi, remembrance, unknown, shinogu, azusa, mupod, dandte, enronjp, zanzura
+randomless, einbroch, sabrekins, baekshi, remembrance, shinogu, azusa, mupod, dandte, zanzura, sabin, emuru[...]ru
 
 **F2P**
 haly, arti, sean, jeff chickendig, strifeser, ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou (F2Dab), nightengale, sadness enchancement ex, zerokool, line x
 
 **EX Rank Luck**
-remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom, gryffinp, sirkenai, qurupeke, saci, david
+remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom, gryffinp, sirkenai, qurupeke, saci, karnapolice, david
 
 **F Rank Luck**
-sabin, furiosa, lavenza, grazzt, laiza`).catch(console.error);
+furiosa, lavenza, grazzt, laiza`).catch(console.error);
 }
