fe6a689c9e06debb21f4b41bdc8b667a160239d5
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-07-03 14:20:05 -0300

    a

--- commands/deleterole.js
+++ commands/deleterole.js
@@ -4,8 +4,9 @@ exports.run = (client, message, args) => {
    //}
   // let myRole = message.guild.roles.find("name", "Notifications");
        if(message.member.roles.has('463714859503058955')){ //If they're on the role, delete them.
+            console.log("Removing " + message.author.username + " from Notifications role.");
             message.member.removeRole('463714859503058955').catch(console.error);
-           message.channel.send("Removed " +message.member.nickname+" from the Notifications role.");
+            message.channel.send("Removed " +message.member.nickname+" from the Notifications role.");
        }
        else{
            message.channel.send(message.member.nickname+", you're not on the role.");
