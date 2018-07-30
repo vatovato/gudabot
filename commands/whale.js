b86034c9f26038f0bbac343a3548e79c42ffc03c
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-07-30 16:28:47 -0300

    a

--- commands/whale.js
+++ commands/whale.js
@@ -3,17 +3,17 @@ exports.run = (client, message, args) => {
 rephin
 
 **Whale Tier**
-ustnap, boat, kayos90, neki (multi-gacha whale), grimmjow, jonjonaug, dark494, randomdude, safflower, pureauthor, t-rex (spook king), unknown, enronjp, noll (mio)
+ustnap, boat, kayos90, grimmjow, jonjonaug, dark494, randomdude, safflower, pureauthor, t-rex (spook king), unknown, enronjp, noll (mio)
 
 **Dolphin Tier**
-randomless, einbroch, sabrekins, baekshi, shinogu, azusa, mupod, dandte, zanzura, sabin, emuru[...]ru, schmitz, jeff chickendig, okimuk
+randomless, einbroch, sabrekins, baekshi, shinogu, azusa, dandte, zanzura, sabin, emuru[...]ru, schmitz, jeff chickendig, okimuk
 
 **F2P**
 haly, arti, strifeser, ibnchi, shinypogs, hubb, wei, ihokeren, fateshirou (F2Dab), nightengale, sadness enchancement ex, zerokool, line x, halation rotation, slashen
 
 **EX Rank Luck**
-remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom, gryffinp, sirkenai, qurupeke, saci, karnapolice, david, chrono, sonicmj
+remembrance, saberkins, xianaphryz, ironraven, hentailover, jommyjomjom, gryffinp, sirkenai, qurupeke, saci, karnapolice, david, chrono, sonicmj, mupod
 
 **F Rank Luck**
-furiosa (spook emperor), lavenza, grazzt, laiza, sean`).catch(console.error);
+furiosa (spook emperor), lavenza, grazzt, laiza, sean, neki (F Rank Luck in every gacha ever)`).catch(console.error);
 }
