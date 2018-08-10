fb05ffb84e5ef631ec046292673546f578f10f7a
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-08-10 11:50:06 -0300

    a

---
+++ commands/degens.js
@@ -0,0 +1,3 @@
+exports.run = (client, message, args) => {
+    message.channel.send("",{files: ["https://media.giphy.com/media/8rElMK4TD4wSun2WBJ/giphy.gif?width=321&height=181"]}).catch(console.error);
+}
