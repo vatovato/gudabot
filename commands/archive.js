c8e255e874c68dcc89d800b9f4b1be8e0f49947f
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-09-18 12:22:31 -0300

    a

--- commands/archive.js
+++ commands/archive.js
@@ -3,15 +3,15 @@ exports.run = (client, message, args) => {
 	var target = 0;
 	target= message.mentions.members.first();
 	console.log("target: " + target); //Finding the victim
-	//console.log(message.author.username + " attempted to archive " + target.nickname);
+	//console.log(message.author.username + " attempted to archive " + target.displayname);
 	if (message.member.hasPermission("KICK_MEMBERS")) { //Check to see if the caller is a mod.
 		if(!target) {message.channel.send("I'm sorry, but that command didn't include a valid @ mention."); return;}//First of all let's see if there was even a valid mention in there.
 		if(!target.hasPermission("KICK_MEMBERS")) {//Check to see if the TARGET is a mod.
 			if(!target.roles.has(ArchivedRole)){//And finally check if the target is already archived
-				message.channel.send(`${target.nickname} has proven to be a troublemaker (or a dabber), and has been summarily archived. Consider this lesson carefully.`);//If the target was unarchived, archive them.
+				message.channel.send(`${target.displayname} has proven to be a troublemaker (or a dabber), and has been summarily archived. Consider this lesson carefully.`);//If the target was unarchived, archive them.
 				target.addRole(ArchivedRole).catch(console.error);
 			}else{//If the target was archived, unarchive them.
-				message.channel.send(target.nickname + " has been released from the Archive. Pending good behavior.");
+				message.channel.send(target.displayname + " has been released from the Archive. Pending good behavior.");
 				target.removeRole(ArchivedRole).catch(console.error);
 			}
 		}else{ //If the target was a mod, taunt the caller about their failure.
@@ -19,7 +19,7 @@ exports.run = (client, message, args) => {
 		}
 	} else { //If the caller is not a mod, then they get slapped.
 		if (message.channel.id != "436192216134844447"){ //If the message is from the archive, ignore it completely.
-			message.channel.send(`${message.author.username} has tried to send someone to the archive while not being a mod, and has thus thrown themselves to the archive. The irony...`);
+			message.channel.send(`${message.member.displayname} has tried to send someone to the archive while not being a mod, and has thus thrown themselves to the archive. The irony...`);
 			message.member.addRole(ArchivedRole).catch(console.error);
 		}
 }
