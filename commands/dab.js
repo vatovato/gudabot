fd232aab13472faa891a30004866738b80af8b94
eugenioteran <eugenioteran@users.noreply.github.com>
2018-10-22 21:41:05 -0300

    a

--- commands/dab.js
+++ commands/dab.js
@@ -1,3 +1,4 @@
 exports.run = (client, message, args) => {
-    message.channel.send("https://i.giphy.com/media/vFSV2lzV0JIHK/giphy.webp").catch(console.error);
+    //message.channel.send("https://i.giphy.com/media/vFSV2lzV0JIHK/giphy.webp").catch(console.error);
+    message.channel.send("https://i.imgur.com/N5A60RW.png").catch(console.error);
 }
