db95dda4021dd6a4908abac52b71aa288353f9ef
eugenioteran <35309563+eugenioteran@users.noreply.github.com>
2018-10-29 08:01:09 -0300

    a

--- commands/master.js
+++ commands/master.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+/*exports.run = (client, message, args) => {
 var request = require('request');
 var cheerio = require('cheerio');
 const Discord = require('discord.js');
@@ -90,4 +90,26 @@ request("http://fate-go.cirnopedia.org/master_mission_us.php#nav", function(erro
     message.channel.send(`${sendMessage}`).catch(console.error);
   }
 });
+}
+*/
+exports.run = (client, message, args) => {
+var request = require('request');
+var cheerio = require('cheerio');
+const Discord = require('discord.js');
+
+var sendMessage = "**Weekly Master Missions**\n\n";
+request("https://grandorder.wiki/Master_Missions", function(error, response, html) {
+  if(!error && response.statusCode == 200) {
+    //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
+    var $ = cheerio.load(html);
+    var tableWithData = $('table[class=wikitable]').first();
+    var dataRows = $(tableWithData).find('td').toArray();
+    for (let i of dataRows) {
+      sendMessage += $(i).text().trim() + "\n";
+
+    }
+    message.channel.send(sendMessage);
+    //console.log(tableWithData.html());
+  }
+  });
 }
