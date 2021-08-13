a6bbdc12598e85cde4a4a2e6154cb39b36852a1e
Sephi <33060221+Sephyz@users.noreply.github.com>
2021-08-13 21:29:56 +0100

    #11 - Migrate servant command to Atlas Academy (#16)

--- commands/scrapeServant.js
+++ commands/scrapeServant.js
@@ -1,137 +1,90 @@
-exports.run = (servantUrl, urlRef, wikiRef, client, message) => {
-var servantCall = servantUrl;
-var urlRef = urlRef;
-var wikiRef = wikiRef;
-var request = require('request');
-var cheerio = require('cheerio');
-const Discord = require('discord.js');
-var servant = {
-  name: '',
-  image: '',
-  serClass: '',
-  rarity: '',
-  baseHP: '',
-  baseATK: '',
-  maxHP: '',
-  maxATK: '',
-  cards: '',
-  np: '',
-  npDesc: ''
-};
+exports.run = (data, client, message) => {
 
-// Retrieves Servant Name from Servant specific URL
-request("http://fate-go.cirnopedia.org/servant_all.php#nav", function(error, response, html) {
-  if(!error && response.statusCode == 200) {
-    //$ = cheerio.load('div', '<div id="mw-content-text">...</div>');
-    var $ = cheerio.load(html);
-    var fullTable = $('tr[id="'+urlRef+'"]');
-    var tablesColumns = $(fullTable).find('td').toArray();
-    var i = 0;
-    for (let tableColumns of tablesColumns){
-      switch (i){
-        case 1:
-        servant.rarity = $(tableColumns).text();
-        break;
-        case 3:
-        servant.name = $(tableColumns).find('a').text();
-        servant.name = servant.name.replace(/[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]|[\uFF00-\uFFEF]|[\u4E00-\u9FAF]|[\u2605-\u2606]|[\u2190-\u2195]|\u203B|\n\r/g, "");
-          break;
-        case 4:
-        servant.serClass = $(tableColumns).text();
-        break;
-        case 5:
-        servant.cost = $(tableColumns).text();
-        break;
-        case 6:
-        servant.baseHP = $(tableColumns).text();
-        break;
-        case 7:
-        servant.baseATK = $(tableColumns).text();
-        break;
-        case 8:
-        servant.maxHP = $(tableColumns).text();
-        break;
-        case 9:
-        servant.maxATK = $(tableColumns).text();
-        break;
-        case 10:
-        var cards = $(tableColumns).find('img').toArray();
-        for(let card of cards) {
-          var cardCheck = $(card).attr('src');
-          if(cardCheck.indexOf("pattern_01") >= 0) {
-            servant.cards += "Quick";
-          } else if (cardCheck.indexOf("pattern_02") >= 0) {
-            servant.cards += "Arts";
-          } else if (cardCheck.indexOf("pattern_03") >= 0) {
-            servant.cards += "Buster";
-          }
-            servant.cards += " ";
-          }
-        break;
-        case 11:
-        var nobleName = $(tableColumns).find('ch1').text();
-        nobleName = nobleName.trim();
-        servant.np = "**Noble Phantasm: " + nobleName;
-        var nobleDesc1 = $(tableColumns).find('ch2').html();
-        nobleDesc1 = nobleDesc1.replace(/<br>/g, "\n");
-        nobleDesc1 = nobleDesc1.replace("&amp;","&");
-        var nobleDesc2 = $(tableColumns).find('ch3').text();
-        nobleDesc2 = nobleDesc2.trim();
-        var noble = $(tableColumns).find('img');
-        var nobleCheck = $(noble).attr('src');
-        if(nobleCheck.indexOf("pattern_01") >= 0) {
-          servant.np += " (Quick)**";
-          servant.npDesc = nobleDesc1 + "\n" + nobleDesc2;
-          } else if (nobleCheck.indexOf("pattern_02") >= 0) {
-          servant.np += " (Arts)**";
-          servant.npDesc = nobleDesc1 + "\n" + nobleDesc2;
-          } else if (nobleCheck.indexOf("pattern_03") >= 0) {
-          servant.np += " (Buster)**";
-          servant.npDesc = nobleDesc1 + "\n" + nobleDesc2;
-          }
-        break;
-        }
-      i++;
+  const Discord = require('discord.js');
+  var servant = {
+    url: '',
+    name: '',
+    image: '',
+    serClass: '',
+    rarity: '',
+    baseHP: '',
+    baseATK: '',
+    maxHP: '',
+    maxATK: '',
+    cards: '',
+    np: '',
+    npDesc: '',
+    growthCurve: '',
+    alignments: '',
+    traits: ''
+  };
+  console.log("Initialise Servant Data");
+
+  servant.url = "https://apps.atlasacademy.io/db/JP/servant/" + data.collectionNo;
+  console.log("Url: "+servant.url);
+
+  servant.name = data.name;
+  console.log("Name: "+servant.name);
+
+  servant.image = data.extraAssets.faces.ascension[4];
+  console.log("Image URL: "+servant.image);
+
+  servant.serClass = data.className;
+  console.log("Class: "+servant.serClass);
+
+  servant.rarity = data.rarity.toString() + "★";
+  console.log("Rarity: "+servant.rarity);
+
+  servant.baseHP = data.hpBase.toString();
+  console.log("BaseHP: "+servant.baseHP);
+
+  servant.baseATK = data.atkBase.toString();
+  console.log("BaseATK: "+servant.baseATK);
+
+  servant.maxHP = data.hpMax.toString();
+  console.log("MaxHP: "+servant.maxHP);
+
+  servant.maxATK = data.atkMax.toString();
+  console.log("MaxATK: "+servant.maxATK);
+
+  servant.cards = `${data.cards[0]}, ${data.cards[1]}, ${data.cards[2]}, ${data.cards[3]}, ${data.cards[4]}`;
+  console.log("Cards: "+servant.cards);
+
+  servant.np = "**Noble Phantasm: " + data.noblePhantasms[data.noblePhantasms.length - 1].name.trim() + "(" + data.noblePhantasms[data.noblePhantasms.length - 1].card + ")**";
+  console.log("NP Name: "+servant.np);
+
+  servant.npDesc = data.noblePhantasms[data.noblePhantasms.length - 1].detail;
+  console.log("NP Descr: "+servant.npDesc);
+
+  servant.growthCurve = data.growthCurve.toString();
+  console.log("Growth Curve: "+servant.growthCurve);
+
+  servant.alignments = "N/A";
+  console.log("Alignments: "+servant.alignments);
+
+  for ( var i = 0; i < data.traits.length; ++i ) {
+    if ( i > 0 ) {
+      servant.traits += ", ";
     }
-    servant.image = "http://fate-go.cirnopedia.org/icons/servant/servant_"+urlRef+"1.png";
+    servant.traits += data.traits[i].name;
+  }
+  console.log("Traits: "+servant.traits);
 
-    request(wikiRef, function(error, response, html) {
-      if(!error && response.statusCode == 200) {
-        var $ = cheerio.load(html);
-       var growthCurve = $('a[href="/wiki/Leveling#Growth_Curves"]').closest('td').text();
-       growthCurve = growthCurve.replace('Growth Curve:', '');
-       growthCurve = growthCurve.trim();
-       console.log("GrowthCurve: "+growthCurve);
+  const embed = new Discord.MessageEmbed()
+  .setTitle(servant.name)
+  .setThumbnail(servant.image)
+  .setURL(servant.url)
+  .addField("Class", servant.serClass, true)
+  .addField("Rarity", servant.rarity, true)
+  .addField("Base HP", servant.baseHP, true)
+  .addField("Base ATK", servant.baseATK, true)
+  .addField("Max HP", servant.maxHP, true)
+  .addField("Max ATK", servant.maxATK, true)
+  .addField("Growth Curve", servant.growthCurve, true)
+  .addField("Alignments",servant.alignments, true)
+  .addField("Traits",servant.traits)
+  .addField("Cards", servant.cards)
+  .addField(servant.np, servant.npDesc)
 
-       var alignments = $('a[href="/wiki/Alignments"]').closest('td').text();
-       alignments = alignments.replace('Alignments:','');
-       alignments = alignments.trim();
-       console.log("Alignments: "+alignments);
-
-       var traits = $('a[href="/wiki/Traits"]').closest('td').first().text();
-       traits = traits.replace('Traits:','');
-       traits = traits.trim();
-       console.log("Traits: "+traits);
-
-       const embed = new Discord.MessageEmbed()
-       .setTitle(servant.name)
-       .setThumbnail(servant.image)
-       .setURL(servantCall)
-       .addField("Class", servant.serClass, true)
-       .addField("Rarity", servant.rarity, true)
-       .addField("Base HP", servant.baseHP, true)
-       .addField("Base ATK", servant.baseATK, true)
-       .addField("Max HP", servant.maxHP, true)
-       .addField("Max ATK", servant.maxATK, true)
-       .addField("Growth Curve", growthCurve, true)
-       .addField("Alignments",alignments, true)
-       .addField("Traits",traits)
-       .addField("Cards", servant.cards)
-       .addField(servant.np, servant.npDesc)
-
-       message.channel.send({embeds: [embed]});
-      }
-    });
-    }
-  });
+  message.channel.send({embeds: [embed]});
 }
