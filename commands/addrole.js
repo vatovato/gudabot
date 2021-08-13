5c31107386fc482f0c4995bef1da02e9c7aa830c
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-13 14:24:41 +0100

    Refactor addrole code and add mafia/genshin roles

--- commands/addrole.js
+++ commands/addrole.js
@@ -10,20 +10,12 @@ exports.run = (client, message, args) => {
 	dict["assassin"]='465552550138216478';
 	dict["berserker"]='465552649446752261';
 	dict["notifications"]='463714859503058955';
+	dict["mafia"]='869280280467349554';
+	dict["genshin"]='832605314728984606';
 
 	var role= args.join(" ").toLowerCase();
 	if ( role in dict && message.guild.roles.cache.get(dict[role]) ) {
 		switch(role){ //Check what role the user requested
-			case "notifications": //Notifications
-				if(message.member.roles.cache.has(dict[role])){ //If they're on the role, do nothing.
-					message.channel.send(message.author.username+", you're already on the role.");
-				}
-				else{
-					console.log("Adding " + message.author.username + " to Notifications role.");
-					message.member.roles.add(dict[role]).catch(console.error); //If they don't, add it.
-					message.channel.send("Added Notifications role to user " + message.author.username+".");
-				}
-				break;
 			case "saber":
 			case "lancer":
 			case "archer":
@@ -31,14 +23,21 @@ exports.run = (client, message, args) => {
 			case "caster":
 			case "assassin":
 			case "berserker": //For all Class Roles
-				if(message.member.roles.cache.has(dict["saber"])||message.member.roles.cache.has(dict["lancer"])||message.member.roles.cache.has(dict["archer"])||message.member.roles.cache.has(dict["rider"])||message.member.roles.cache.has(dict["caster"])||message.member.roles.cache.has(dict["assassin"])||message.member.roles.cache.has(dict["berserker"])){//Check if the user has a Class Role already.
+				if(message.member.roles.cache.has(dict["saber"])||message.member.roles.cache.has(dict["lancer"])||message.member.roles.cache.has(dict["archer"])||message.member.roles.cache.has(dict["rider"])||message.member.roles.cache.has(dict["caster"])||message.member.roles.cache.has(dict["assassin"])||message.member.roles.cache.has(dict["berserker"])){
+					//If the user has a Class Role already, break out of the switch case early.
 					message.channel.send(message.author.username+", you already have a Class.");
+					break;
 				}
-				else{ //If the user does not have a class, then add them to the requested class.
-						console.log("Adding " + message.author.username + " to " +  message.guild.roles.cache.find(dict[role]).name + " role.");
-						message.member.roles.add(dict[role]).catch(console.error); //Make the user a Saber.
-						message.channel.send("Added " +  message.guild.roles.cache.find(dict[role]).name + " role to user " + message.author.username+".");
-						break;
+			case "notifications": //Notifications
+			case "mafia": //Mafia
+			case "genshin": //Genshin
+				if(message.member.roles.cache.has(dict[role])){ //If they're on the role, do nothing.
+					message.channel.send(message.author.username+", you're already on the role.");
+				}
+				else{
+					console.log("Adding " + message.author.username + " to  " + message.guild.roles.cache.find(dict[role]).name + " role.");
+					message.member.roles.add(dict[role]).catch(console.error); //If they don't, add it.
+					message.channel.send("Added " + message.guild.roles.cache.find(dict[role]).name + " role to user " + message.author.username+".");
 				}
 				break;
 			default: //when the args are not a valid role
