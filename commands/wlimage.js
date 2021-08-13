0aa8b6b1f005cb1f4fb4ba2eb40f690e8778cd9b
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-13 18:10:18 +0100

    Migrating wlimage to use atlas academy

--- commands/wlimage.js
+++ commands/wlimage.js
@@ -1,8 +1,5 @@
 exports.run = (client, message, connection, args) => {
 
-
-  console.log(`Test wlimage`);
-
   var userID = message.author.id;
   var username = message.author.username;
   var nickname = message.member.nickname;
@@ -14,9 +11,6 @@ exports.run = (client, message, connection, args) => {
   var invalidServantName = args.join("+").toLowerCase();
   var searchUrl = `https://www.google.com.ar/search?q= ${invalidServantName} +site%3Ahttps%3A%2F%2Fapps.atlasacademy.io`;
 
-  //URL for the servant called in servantName
-  var servantUrl ='';
-
   console.log(`Required adding ${servantName} image to ${username}'s wishlist.`);
 
   fetch('https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json')
@@ -24,15 +18,13 @@ exports.run = (client, message, connection, args) => {
       .then(data => {
         data.forEach((item) => {
           if ( item.name.toLowerCase() == servantName ) {
-            servantUrl = item.extraAssets.faces.ascension[4];
-            message.channel.send(`Link to Atlas Academy: ${servantUrl}`);
             connection.query(`SELECT * FROM wishlist WHERE userID = '${userID}'`, function(err, rows, fields) {
               if (err) throw err;
               if(rows.length == 0) {
                 console.log("User did not exist.");
                 message.channel.send(`**${username}**, you don't exist in the table yet. Use !addwl [wishlist] first.`);
               } else {
-                connection.query(`UPDATE wishlist SET imageURL = '${servantUrl}' WHERE userID = ${userID}`);
+                connection.query(`UPDATE wishlist SET imageURL = '${item.extraAssets.faces.ascension[4]}' WHERE userID = ${userID}`);
                 message.channel.send(`${username}, you added your image. Call !wl to see it.`);
               }
             });
