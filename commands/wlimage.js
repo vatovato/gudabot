da5a26ea80aa42b7e8d6676a29f234af658bf5ba
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-13 18:22:40 +0100

    Fix invalid servant warning being sent unnecessarily

--- commands/wlimage.js
+++ commands/wlimage.js
@@ -16,21 +16,21 @@ exports.run = (client, message, connection, args) => {
   fetch('https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json')
     .then(response => response.json())
       .then(data => {
-        data.forEach((item) => {
-          if ( item.name.toLowerCase() == servantName ) {
+        for ( var i = 0; i < data.length; ++i ) {
+          if ( data[i].name.toLowerCase() == servantName ) {
             connection.query(`SELECT * FROM wishlist WHERE userID = '${userID}'`, function(err, rows, fields) {
               if (err) throw err;
               if(rows.length == 0) {
                 console.log("User did not exist.");
                 message.channel.send(`**${username}**, you don't exist in the table yet. Use !addwl [wishlist] first.`);
               } else {
-                connection.query(`UPDATE wishlist SET imageURL = '${item.extraAssets.faces.ascension[4]}' WHERE userID = ${userID}`);
+                connection.query(`UPDATE wishlist SET imageURL = '${data[i].extraAssets.faces.ascension[4]}' WHERE userID = ${userID}`);
                 message.channel.send(`${username}, you added your image. Call !wl to see it.`);
               }
             });
             return;
           }
-        })
+        }
         message.channel.send(`Invalid Servant name. Try Google: ${searchUrl}`);
       })
   
