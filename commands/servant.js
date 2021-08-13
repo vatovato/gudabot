038d3899aa4db1196968cfcb112449c2b4f7f22f
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-13 21:01:54 +0100

    Migrate from Cirno to Atlas Academy

--- commands/servant.js
+++ commands/servant.js
@@ -1,22 +1,29 @@
 
 exports.run = (client, message, args) => {
 
-  var request = require('request');
-  var cheerio = require('cheerio');
-  const Discord = require('discord.js');
-//Concatenates all args to form the servant name called for the function
+  const fetch = require('node-fetch');
+
+  //Concatenates all args to form the servant name called for the function
   var servantName = args.join(" ").toLowerCase();
   var invalidServantName = args.join("+").toLowerCase();
-  var searchUrl = "https://www.google.com.ar/search?q=" + invalidServantName + "+site%3Ahttp%3A%2F%2Ffate-go.cirnopedia.org";
-  //Stores the 3 digit code that identifies a Servant in cirno
-  var urlRef = '';
-  var wikiRef = '';
-  //URL for the servant called in servantName
-  var servantUrl ='';
-  var client = client;
-  var message = message;
-  console.log(`Required cirno page for ${servantName}`);
+  var searchUrl = "https://www.google.com.ar/search?q=" + invalidServantName + "+site%3Ahttps%3A%2F%2Fapps.atlasacademy.io";
 
+  console.log(`Required adding ${servantName} image to ${username}'s wishlist.`);
+
+  fetch('https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json')
+    .then(response => response.json())
+      .then(data => {
+        for ( var i = 0; i < data.length; ++i ) {
+          if ( data[i].name.toLowerCase() == servantName ) {
+            let commandFile2 = require(`./scrapeServant.js`);
+            commandFile2.run(data[i], client, message);
+            return;
+          }
+        }
+        message.channel.send(`Invalid Servant name. Try Google: ${searchUrl}`);
+      })
+
+    /*
     var callSwitch  = require(`./switch.js`);
     var returnValue = callSwitch.parseName(servantName);
     returnValue = returnValue.split(" ");
@@ -30,5 +37,6 @@ exports.run = (client, message, args) => {
     servantUrl = 'http://fate-go.cirnopedia.org/servant_profile.php?servant=' + urlRef;
     let commandFile2 = require(`./scrapeServant.js`);
     commandFile2.run(servantUrl, urlRef, wikiRef, client, message);
-  }
+    }
+  */
 }
