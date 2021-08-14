d85ee3c9f7cae09ee1f74d230d62154a21bd1513
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-14 14:09:47 +0100

    Add anime and manga search commands

--- commands/kitsu.js
+++ commands/kitsu.js
@@ -1,65 +1,69 @@
 exports.run = (client, message, args) => {
-  const Discord = require('discord.js');
-  const fetch = require('node-fetch');
+	const Discord = require('discord.js');
+	const fetch = require('node-fetch');
 
-  // Possible arguments
-  const kitsuCommands = {
+	// Possible arguments
+	const kitsuCommands = {
 	'help': 'help',
 	'anime': 'anime anime_name', 
 	'manga': 'manga manga_name'
-  }
-  const kitsuDetails = {
-	'help': 'Returns a list of available commands',
+	}
+	const kitsuDetails = {
+	'help': 'Shows a list of available commands',
 	'anime': 'Returns an anime that matches the search prompt', 
 	'manga': 'Returns a manga that matches the search prompt'
-  }
+	}
 
-  if ( args.length && args[0].toLowerCase() in kitsuCommands ) {
-
-		switch(args[0].toLowerCase()) {
-			case 'help':
-				const embed = new Discord.MessageEmbed()
-				.setTitle("Kitsu Commands")
-				.setThumbnail("https://kitsu.io/kitsu-256-ed442f7567271af715884ca3080e8240.png")
-				.setURL("https://kitsu.io/");
-				for ( const [key, value] of Object.entries(kitsuCommands) ) {
-					embed.addField(value,kitsuDetails[key]);
-				}
-				message.channel.send({embeds: [embed]});
-				break;
-			case 'anime':
-				break;
-			case 'manga':
-				break;
-			default:
-				break;
-		}
-		/*
-		//Concatenates all args to form the servant name called for the function
-		var servantName = args.join(" ").toLowerCase();
-		var invalidServantName = args.join("+").toLowerCase();
-		var searchUrl = "https://www.google.com.ar/search?q=" + invalidServantName + "+site%3Ahttps%3A%2F%2Fapps.atlasacademy.io";
-
-		fetch('https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json')
-		.then(response => response.json())
-			.then(data => {
-			for ( var i = 0; i < data.length; ++i ) {
-				if ( data[i].name.toLowerCase() == servantName ) {
-				let commandFile2 = require(`./scrapeServant.js`);
-				commandFile2.run(data[i], client, message);
-				return;
-				}
+	if ( args.length ) {
+		const commandString = args.shift();
+		if ( commandString in kitsuCommands ) {
+			switch(commandString.toLowerCase()) {
+				case 'help':
+					const embed = new Discord.MessageEmbed()
+					.setTitle("Kitsu Commands")
+					.setThumbnail("https://kitsu.io/kitsu-256-ed442f7567271af715884ca3080e8240.png")
+					.setURL("https://kitsu.io/");
+					for ( const [key, value] of Object.entries(kitsuCommands) ) {
+						embed.addField(value,kitsuDetails[key]);
+					}
+					message.channel.send({embeds: [embed]});
+					break;
+				case 'anime':
+				case 'manga':
+					//Concatenates all remaining args to form the search prompt
+					const searchPrompt = encodeURIComponent(args.join(" "));
+					var searchUrl = "https://kitsu.io/api/edge/" + commandString + "?" + searchPrompt;
+					
+					fetch(searchUrl)
+					.then(response => response.json())
+						.then(data => {
+							if ( data.length ) {
+								const bestResult = data[0].attributes;
+								const embed = new Discord.MessageEmbed()
+								.setTitle(bestResult.canonicalTitle + "(" + bestResult.startDate.slice(0, 4) + ")")
+								.setThumbnail(bestResult.posterImage.tiny)
+								.setURL("https://kitsu.io/" + commandString + "/" + bestResult.slug)
+								.addField("Popularity Rank", bestResult.popularityRank, true)
+								.addField("Rating Rank", bestResult.ratingRank, true)
+								.addField("Approval", bestResult.averageRating + "%", true)
+								.addField("Age Rating", bestResult.ageRating, true)
+								.addField("Synopsis", bestResult.synopsis.length > 1000 ? bestResult.synopsis.substring(0, 997) + "..." : bestResult.synopsis)
+								message.channel.send({embeds: [embed]});
+							}
+							else {
+								message.channel.send(`Kitsu: couldn't find a result with the search term "${commandString}"`);
+							}
+						})
+					break;
+				default:
+					break;
 			}
-			message.channel.send(`Invalid Servant name. Try Google: ${searchUrl}`);
-			})*/
-  } else {
-		var errorString = '';
-		if ( args.length ) {
-			errorString += `Kitsu: ${args[0].toLowerCase()} argument not recognised.`;
-		} else {
-			errorString += `Kitsu: no argument found.`;
 		}
-		errorString += "\nUse '!kitsu help' for a list of available commands."
-		message.channel.send(errorString);
-  }
+		else {
+			message.channel.send(`Kitsu: command ${commandString} not recognised.\nUse '!kitsu help' for a list of available commands.`);
+		}
+	} 
+	else {
+		message.channel.send("Kitsu: no argument found.\nUse '!kitsu help' for a list of available commands.");
+	}
 }
