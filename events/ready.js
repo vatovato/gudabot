46abde29ca7aad08047290dceb01033f1aa67179
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-14 21:55:11 +0100

    Fix onReady console log

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 exports.run = (client) => {
-    console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users`);
+    console.log(`Ready to server in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 }
