180357b99a2f1aadc3ebf06d293379552de5f20d
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-15 00:09:59 +0100

    Initial implementation of kitsu user command

--- commands/kitsu.js
+++ commands/kitsu.js
@@ -60,10 +60,10 @@ async function handleKitsuCommand(message, commandString, args) {
 						console.log("Querying " + data.data[0].relationships.genres.links.related);
 						const genreResponse = await fetch(data.data[0].relationships.genres.links.related);
 						const genreData = await genreResponse.json();
-						createEmbed(message, commandString, data.data[0].attributes, genreData.data);
+						createAnimeEmbed(message, commandString, data.data[0].attributes, genreData.data);
 					}
 					else {
-						message.channel.send(`Kitsu: couldn't find a result with the search term "${searchPrompt}"`);
+						message.channel.send(`Kitsu: Couldn't find a result with the search term "${searchPrompt}"`);
 					}
 				} catch(err) {
 					console.log(err);
@@ -88,7 +88,7 @@ async function handleKitsuCommand(message, commandString, args) {
 					const genreResponse = await fetch(randItem.data[0].relationships.genres.links.related);
 					const genreData = await genreResponse.json();
 
-					createEmbed(message, commandString, randItem.data[0].attributes, genreData.data);
+					createAnimeEmbed(message, commandString, randItem.data[0].attributes, genreData.data);
 
 				} catch(err) {
 					console.log(err);
@@ -96,13 +96,52 @@ async function handleKitsuCommand(message, commandString, args) {
 			}
 			break;
 		case 'user':
+			//Concatenates all remaining args to form the search prompt, if there are any
+			const searchPrompt = encodeURIComponent(args.join(" "));
+			if ( searchPrompt.length ) {
+				var searchUrl = "https://kitsu.io/api/edge/users?filter[text]=" + searchPrompt;
+
+				console.log("Querying " + searchUrl);
+				try {
+					const response = await fetch(searchUrl);
+					const data = await response.json();
+
+					console.log("Found " + data.meta.count.toString() + " results");
+					if ( data.meta.count > 0 ) {
+						
+						// Query waifu data
+						console.log("Querying " + data.data[0].relationships.waifu.links.related);
+						const waifuResponse = await fetch(data.data[0].relationships.waifu.links.related);
+						const waifuData = await waifuResponse.json();
+						// Query stats data
+						console.log("Querying " + data.data[0].relationships.stats.links.related);
+						const statsResponse = await fetch(data.data[0].relationships.stats.links.related);
+						const statsData = await statsResponse.json();
+						// Query favorites data
+						console.log("Querying " + data.data[0].relationships.favorites.links.related);
+						const favoritesResponse = await fetch(data.data[0].relationships.favorites.links.related);
+						const favoritesData = await favoritesResponse.json();
+
+						createAnimeEmbed(message, commandString, data.data[0].attributes, waifuData.data, statsData.data, favoritesData.data);
+					}
+					else {
+						message.channel.send(`Kitsu: Couldn't find a user called "${searchPrompt}"`);
+					}
+				} catch(err) {
+					console.log(err);
+				}
+			}
+			else {
+				message.channel.send(`Kitsu: No username has been specified.`);
+			}
+			break;
 		default:
 			break;
 	}
 }
 
-// Handle JSON data and embed message here
-function createEmbed(message, type, item, genres = null) {
+// Handle JSON data and embed anime/manga message here
+function createAnimeEmbed(message, type, item, genres = null) {
 	const Discord = require('discord.js');
 
 	const contentFilter = {
@@ -111,38 +150,97 @@ function createEmbed(message, type, item, genres = null) {
 	'yaoi': 3
 	}
 
-	switch(type) {
-		case 'anime':
-		case 'manga':
-			// Parse through genre JSON
-			var genreString = '';
-			if ( genres != null ) {
-				for (var i = 0; i < genres.length; ++i) {
-					genreString += (i > 0 ? ", " : "" ) + genres[i].attributes.name;
-					if ( genres[i].attributes.name.toLowerCase() in contentFilter ) {
-						message.channel.send(`Kitsu: Hentai content has been disabled, search result cannot be displayed.`);
-						return;
-					}
-				}
-			} else {
-				console.log("No genres found");
+	// Parse through genre JSON
+	var genreString = '';
+	if ( genres != null ) {
+		for (var i = 0; i < genres.length; ++i) {
+			genreString += (i > 0 ? ", " : "" ) + genres[i].attributes.name;
+			if ( genres[i].attributes.name.toLowerCase() in contentFilter ) {
+				message.channel.send(`Kitsu: Hentai content has been disabled, search result cannot be displayed.`);
+				return;
 			}
+		}
+	} else {
+		console.log("No genres found");
+	}
 
-			// Send embed to channel
-			const embed = new Discord.MessageEmbed()
-			.setTitle(item.canonicalTitle + " (" + (item.startDate ? item.startDate.slice(0, 4) : "N/A") + ")")
-			.setThumbnail(item.posterImage.tiny)
-			.setURL("https://kitsu.io/" + type + "/" + item.slug)
-			.addField("Popularity Rank", item.popularityRank ? item.popularityRank.toString() : "N/A", true)
-			.addField("Rating Rank", item.ratingRank ? item.ratingRank.toString() : "N/A", true)
-			.addField("Approval", item.averageRating ? item.averageRating + "%" : "N/A", true)
-			.addField("Genres", genreString.length ? genreString : "N/A", true)
-			.addField("Age Rating", item.ageRating ? item.ageRating + (item.ageRatingGuide ? "- " + item.ageRatingGuide : "") : "N/A", true)
-			.addField("Status", item.status ? item.status[0].toUpperCase() + item.status.substring(1) : "N/A", true)
-			.addField("Synopsis", item.synopsis && item.synopsis.trim() ? (item.synopsis.length > 1000 ? item.synopsis.substring(0, 997) + "..." : item.synopsis) : "N/A")
-			message.channel.send({embeds: [embed]});
-		case 'user':
-		default:
-			break;
+	// Send embed to channel
+	const embed = new Discord.MessageEmbed()
+	.setTitle(item.canonicalTitle + " (" + (item.startDate ? item.startDate.slice(0, 4) : "N/A") + ")")
+	.setThumbnail(item.posterImage.tiny)
+	.setURL("https://kitsu.io/" + type + "/" + item.slug)
+	.addField("Popularity Rank", item.popularityRank ? item.popularityRank.toString() : "N/A", true)
+	.addField("Rating Rank", item.ratingRank ? item.ratingRank.toString() : "N/A", true)
+	.addField("Approval", item.averageRating ? item.averageRating + "%" : "N/A", true)
+	.addField("Genres", genreString.length ? genreString : "N/A", true)
+	.addField("Age Rating", item.ageRating ? item.ageRating + (item.ageRatingGuide ? "- " + item.ageRatingGuide : "") : "N/A", true)
+	.addField("Status", item.status ? item.status[0].toUpperCase() + item.status.substring(1) : "N/A", true)
+	.addField("Synopsis", item.synopsis && item.synopsis.trim() ? (item.synopsis.length > 1000 ? item.synopsis.substring(0, 997) + "..." : item.synopsis) : "N/A")
+	message.channel.send({embeds: [embed]});
+	}
+}
+
+// Handle JSON data and embed user message here
+function createUserEmbed(message, type, item, waifu = null, stats = null, favourites = null) {
+	const Discord = require('discord.js');
+
+	// Parse through anime categories
+	var animeCategoryString = 'N/A';
+	var animeCategoryCount = 0;
+	if ( stats != null ) {
+		for (const genre in stats[1].attributes.statsData.categories) {
+			if ( stats[1].attributes.statsData.categories[genre] > animeCategoryCount ) {
+				animeCategoryCount = stats[1].attributes.statsData.categories[genre];
+				animeCategoryString = genre;
+			}
+		}
+	} else {
+		console.log("No genres found");
+	}
+	// Parse through manga categories
+	var mangaCategoryString = 'N/A';
+	var mangaCategoryCount = 0;
+	if ( stats != null ) {
+		for (const genre in stats[3].attributes.statsData.categories) {
+			if ( stats[1].attributes.statsData.categories[genre] > mangaCategoryCount ) {
+				mangaCategoryCount = stats[3].attributes.statsData.categories[genre];
+				mangaCategoryString = genre;
+			}
+		}
+	} else {
+		console.log("No genres found");
+	}
+	// Parse through favourite anime
+	var mangaCategoryString = 'N/A';
+	var mangaCategoryCount = 0;
+	if ( stats != null ) {
+		for (const genre in stats[3].attributes.statsData.categories) {
+			if ( stats[1].attributes.statsData.categories[genre] > mangaCategoryCount ) {
+				mangaCategoryCount = stats[3].attributes.statsData.categories[genre];
+				mangaCategoryString = genre;
+			}
+		}
+	} else {
+		console.log("No genres found");
+	}
+
+	// Send embed to channel
+	const embed = new Discord.MessageEmbed()
+	.setTitle(item.name)
+	.setThumbnail(item.avatar ? item.avatar : "https://kitsu.io/kitsu-256-ed442f7567271af715884ca3080e8240.png")
+	.setURL("https://kitsu.io/users/" + item.slug)
+	.addField("Waifu", waifu ? waifu.canonicalName : "N/A", true)
+	.addField("Anime Finished", stats ? stats[0].attributes.statsData.completed : "0", true)
+	.addField("Most Watched Genre", animeCategoryString + " (" + animeCategoryCount.toString() + ")", true)
+	.addField("Content Rated", item.ratingsCount.toString(), true)
+	.addField("Manga Finished", stats ? stats[2].attributes.statsData.completed : "0", true)
+	.addField("Most Read Genre", mangaCategoryString + " (" + mangaCategoryCount.toString() + ")", true)
+	.addField("Favorites", "N/A")
+
+	if ( waifu ) {
+		embed.setImage(waifu.image.original);
+	}
+
+	message.channel.send({embeds: [embed]});
 	}
 }
