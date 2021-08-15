22365b7faf32f69796c02d271b7575fe8732c9ad
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-15 23:19:40 +0100

    Update fantasy.js

--- commands/fantasy.js
+++ commands/fantasy.js
@@ -69,8 +69,7 @@ async function handleFantasyCommand(message, commandString, args) {
 					if ( data ) {
 						console.log("Fantasy: " + data.events.length.toString() + " events found.");
 						for ( var i = 0; i < data.events.length; ++i ) {
-							console.log("Fantasy: Timestamp is" + data.events[i].deadline_time_epoch.toString());
-							if ( data.events[i].deadline_time_epoch > currentTime ) { // Find the first gameweek in the future 
+							if ( data.events[i].deadline_time_epoch * 1000 > currentTime ) { // Find the first gameweek in the future 
 								message.channel.send("The next transfer deadline is <t:" + data.events[i].deadline_time_epoch.toString() + ":R> (Hover your cursor over the countdown for a timestamp)");
 								return;
 							}
