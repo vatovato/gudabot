34a519a83da32ba4da55103b4ed80025cb9dab7b
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-16 16:26:43 +0100

    Update twitfix.js

--- plugins/twitfix.js
+++ plugins/twitfix.js
@@ -3,31 +3,27 @@ exports.run = (client, message) => {
     const fetch = require('node-fetch');
     const regex = /(https?:\/\/twitter[^\s]+)/g; // Regex to find all twitter links
 
-    const linksArray = message.content.match(regex);
-    var searchUrl = "https://api.twitter.com/2/tweets?ids=";
-
+    const linksArray = message.content.match(regex); // All pattern matches in an array of strings
+    var searchUrl = "https://api.twitter.com/2/tweets?ids="; // Twitter API v2
     var fixedLinks = {}; // id to fxtwitter link lookup
 
     for ( var i = 0; i < linksArray.length; ++i ) {
-        console.log("String (" + linksArray[i] + ") matched. Checking tweet content..." );
-
         const twitterID = linksArray[i].replace(/\?.*$/,"").split('/').pop(); // Remove ?s= at the end, split with / and take the last element
 
         if ( twitterID && twitterID.length ) {
             fixedLinks[twitterID] = linksArray[i].replace("https://twitter.com/","https://fxtwitter.com/");
-            console.log("Twitter ID is (" + twitterID.toString() + ")" );
-            console.log(fixedLinks);
-
+            //console.log("Twitter ID is (" + twitterID.toString() + ")" );
             if ( i > 0 ) {
                 searchUrl += ",";     
 		    }
             searchUrl += twitterID;
         }
 	}
+    //console.log(fixedLinks);
         
     searchUrl += "&expansions=attachments.media_keys";
 
-	console.log("Querying twitter api for tweet info (" + searchUrl + ")");
+	//console.log("Querying twitter api for tweet info (" + searchUrl + ")");
     fetch(searchUrl, {
         method: 'GET',
         headers: {
@@ -50,7 +46,8 @@ exports.run = (client, message) => {
 			}     
 		}
 
-        var newMessage = "Found " + twitterIDs.length + " tweets with video content.\n";
+        // Add all tweets with links to the bot message
+        var newMessage = "Found " + twitterIDs.length + " tweet" + ( twitterIDs.length > 1 ? "s" : "") + " with video content.\n";
         for ( var l = 0; l < twitterIDs.length; ++l ) {
             if ( twitterIDs[l] in fixedLinks ) {
                 newMessage += fixedLinks[twitterIDs[l]] + "\n";
