54dfefe9358827bcb445f5e23f9bb91ec4013fb5
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-17 13:03:02 +0100

    Update pagination.js

--- plugins/pagination.js
+++ plugins/pagination.js
@@ -54,7 +54,7 @@ const paginationEmbed = async (msg, pages, authorOnly = false, timeout = 120000,
   console.log(msg.author.id);
   
   const filter = (i) => {
-    ( !authorOnly || i.user.id === msg.author.id ) &&
+    ( i.user.id === msg.author.id ) &&
     ( i.customId === buttonList[0].customId || i.customId === buttonList[1].customId )
    };
 
