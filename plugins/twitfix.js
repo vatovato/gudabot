956239c8797590e730367b75938e5c99d72996ac
Sephyz <33060221+Sephyz@users.noreply.github.com>
2021-08-17 11:30:56 +0100

    Update twitfix.js

--- plugins/twitfix.js
+++ plugins/twitfix.js
@@ -46,7 +46,7 @@ exports.run = (client, message) => {
 				}
 			}
             else if ( data.includes.media[j].type == "photo" ) {
-                console.log(data.includes.media[j]);
+                //console.log(data.includes.media[j]);
                 imageEmbeds.push(data.includes.media[j].url);
 			}
 		}
