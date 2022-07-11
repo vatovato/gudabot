ba3a248862b4abc86599460a73fff87217502d9f
vatovato <35309563+vatovato@users.noreply.github.com>
2022-07-11 09:37:06 -0300

    Update jannu.js

--- commands/jannu.js
+++ commands/jannu.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
 var args = args.toString().toLowerCase();
 if(args == "alter") {
-  message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310765734690828/jannu_alter.gif").catch(console.error);
+  message.channel.send("https://i.imgur.com/Y0Aww1u.gif").catch(console.error);
   return;
 }
 message.channel.send("https://i.imgur.com/K2qo3at.gif").catch(console.error);
