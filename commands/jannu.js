637e72407c858cc7dacb5ec688ab132987a233a4
vatovato <35309563+vatovato@users.noreply.github.com>
2022-07-11 09:34:05 -0300

    Update jannu.js

--- commands/jannu.js
+++ commands/jannu.js
@@ -4,6 +4,6 @@ if(args == "alter") {
   message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310765734690828/jannu_alter.gif").catch(console.error);
   return;
 }
-message.channel.send("https://cdn.discordapp.com/attachments/404408579144286209/425310265844695040/jannu.gif").catch(console.error);
+message.channel.send("https://i.imgur.com/K2qo3at.gif").catch(console.error);
 }
 //Update: deleted jannu fast
