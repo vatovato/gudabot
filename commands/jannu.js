a6588bf26948073d774b9e2ee7a79675059a4948
vatovato <35309563+vatovato@users.noreply.github.com>
2022-07-11 10:56:27 -0300

    Update jannu.js

--- commands/jannu.js
+++ commands/jannu.js
@@ -1,9 +1,9 @@
 exports.run = (client, message, args) => {
 var args = args.toString().toLowerCase();
 if(args == "alter") {
-  message.channel.send("https://i.imgur.com/Y0Aww1u.gif").catch(console.error);
+  message.channel.send("https://media.discordapp.net/attachments/590423916049334292/996051822294863942/W2JdScs.gif").catch(console.error);
   return;
 }
-message.channel.send("https://i.imgur.com/K2qo3at.gif").catch(console.error);
+message.channel.send("https://media.discordapp.net/attachments/590423916049334292/996052255834910850/0fcaa414565842cdc4be2d9492c38803.gif").catch(console.error);
 }
 //Update: deleted jannu fast
